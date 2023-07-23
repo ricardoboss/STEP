ad62adee14e8ed3d5d3120153c337d211dcfff38
Ricardo Boss <contact@ricardoboss.de>
2023-07-24 00:41:01 +0200

    Extracted lots of logic out of ParseAsync into many, smaller functions

--- HILFE/Parsing/Parser.cs
+++ HILFE/Parsing/Parser.cs
@@ -28,150 +28,15 @@ public class Parser
             cancellationToken.ThrowIfCancellationRequested();
 
             if (token.Type == TokenType.TypeName)
-            {
-                // variable declaration: <type name> <identifier> = <expression>
-                var identifier = Expect(TokenType.Identifier);
-                Expect(TokenType.EqualsSymbol);
-                var expressionTokens = ReadUntil(TokenType.NewLine);
-                Expect(TokenType.NewLine);
-
-                var expression = ExpressionParser.Parse(expressionTokens);
-                yield return new VariableDeclarationStatement(token, identifier, expression);
-            }
+                yield return ParseVariableDeclaration(token);
             else if (token.Type == TokenType.Identifier)
-            {
-                var nextType = tokenQueue.PeekType();
-                switch (nextType)
-                {
-                    case TokenType.ExpressionOpener:
-                    {
-                        // function call: <identifier>([<expression>[, <expression>]*])
-                        Expect(TokenType.ExpressionOpener);
-                        var argsTokens = ReadUntil(TokenType.ExpressionCloser);
-                        Expect(TokenType.ExpressionCloser);
-
-                        var args = ExpressionParser.ParseExpressions(argsTokens).ToList();
-                        yield return new FunctionCallStatement(token, args);
-
-                        break;
-                    }
-                    case { } when nextType.IsMathematicalOperation():
-                    {
-                        // shortcut setter: <identifier>++ / <identifier>-- / <identifier> *= <expression> / <identifier> /= <expression>
-                        var operationToken = GetNext();
-
-                        var valueDenominator = Expect(TokenType.PlusSymbol,
-                            TokenType.MinusSymbol, TokenType.AsteriskSymbol, TokenType.SlashSymbol,
-                            TokenType.PercentSymbol, TokenType.EqualsSymbol);
-
-                        IReadOnlyList<Token> valueExpTokens;
-                        if (valueDenominator.Type.IsMathematicalOperation())
-                        {
-                            // mathematical operation must be the same type as the initial operation (i.e. +- or *- is forbidden)
-                            if (operationToken.Type != valueDenominator.Type)
-                                throw new UnexpectedTokenException(valueDenominator, operationToken.Type);
-
-                            valueExpTokens = new Token[] { new(TokenType.LiteralNumber, "1") };
-                        }
-                        else
-                        {
-                            Debug.Assert(valueDenominator.Type is TokenType.EqualsSymbol);
-
-                            valueExpTokens = ReadUntil(TokenType.NewLine);
-                            Expect(TokenType.NewLine);
-                        }
-
-                        // TODO: ugly. Move expression parsing to another class
-                        var expression = ExpressionParser.Parse(new[] {token, operationToken}.Concat(valueExpTokens).ToArray());
-
-                        yield return new VariableAssignmentStatement(token, expression);
-
-                        break;
-                    }
-                    default:
-                    {
-                        // variable assignment: <identifier> = <expression>
-                        Expect(TokenType.EqualsSymbol);
-                        var expressionTokens = ReadUntil(TokenType.NewLine);
-                        Expect(TokenType.NewLine);
-
-                        var expression = ExpressionParser.Parse(expressionTokens);
-                        yield return new VariableAssignmentStatement(token, expression);
-
-                        break;
-                    }
-                }
-            }
+                yield return ParseIdentifierUsage(token);
             else if (token.Type == TokenType.IfKeyword)
-            {
-                // branching: if (<expression>) { [statement]* } [else[if (<expression>)] { [statement]* } ]
-
-                Expect(TokenType.ExpressionOpener);
-                var expressionTokens = ReadUntil(TokenType.ExpressionCloser);
-                Expect(TokenType.ExpressionCloser);
-
-                Expect(TokenType.CodeBlockOpener);
-                var trueBranchTokens = ReadUntil(TokenType.CodeBlockCloser);
-                Expect(TokenType.CodeBlockCloser);
-
-                var condition = ExpressionParser.Parse(expressionTokens);
-                var statements = await ParseStatements(trueBranchTokens, cancellationToken).ToListAsync(cancellationToken);
-
-                if (tokenQueue.PeekType() is not TokenType.ElseKeyword)
-                {
-                    yield return new IfStatement(condition, statements);
-                }
-                else
-                {
-                    Expect(TokenType.ElseKeyword);
-
-                    IReadOnlyList<Token>? elseExpressionTokens = null;
-                    if (tokenQueue.PeekType() is TokenType.IfKeyword)
-                    {
-                        Expect(TokenType.ExpressionOpener);
-                        elseExpressionTokens = ReadUntil(TokenType.ExpressionCloser);
-                        Expect(TokenType.ExpressionCloser);
-                    }
-
-                    Expect(TokenType.CodeBlockOpener);
-                    var falseBranchTokens = ReadUntil(TokenType.CodeBlockCloser);
-                    Expect(TokenType.CodeBlockCloser);
-
-                    var elseExpression =
-                        elseExpressionTokens is not null ? ExpressionParser.Parse(elseExpressionTokens) : null;
-
-                    var elseStatements = await ParseStatements(falseBranchTokens, cancellationToken)
-                        .ToListAsync(cancellationToken);
-
-                    yield return new IfElseStatement(condition, statements, elseExpression, elseStatements);
-                }
-            }
+                yield return await ParseIfStatement(cancellationToken);
             else if (token.Type == TokenType.WhileKeyword)
-            {
-                // looping: while (<expression>) { [statement]* }
-                Expect(TokenType.ExpressionOpener);
-                var expressionTokens = ReadUntil(TokenType.ExpressionCloser);
-                Expect(TokenType.ExpressionCloser);
-
-                Expect(TokenType.CodeBlockOpener);
-                var statementsTokens = ReadUntil(TokenType.CodeBlockCloser);
-                Expect(TokenType.CodeBlockCloser);
-
-                var condition = ExpressionParser.Parse(expressionTokens);
-                var statements = await ParseStatements(statementsTokens, cancellationToken).ToListAsync(cancellationToken);
-
-                yield return new WhileStatement(condition, statements);
-            }
+                yield return await ParseWhileLoop(cancellationToken);
             else if (token.Type == TokenType.CodeBlockOpener)
-            {
-                // anonymous code block: { [statement]* }
-                var statementsTokens = ReadUntil(TokenType.CodeBlockCloser);
-                Expect(TokenType.CodeBlockCloser);
-
-                var statements = await ParseStatements(statementsTokens, cancellationToken).ToListAsync(cancellationToken);
-
-                yield return new AnonymousCodeBlockStatement(statements);
-            }
+                yield return await ParseAnonymousCodeBlock(cancellationToken);
             else if (token.Type == TokenType.CodeBlockCloser)
                 yield break;
             else if (token.Type is not TokenType.Whitespace and not TokenType.NewLine)
@@ -179,9 +44,167 @@ public class Parser
         }
 
         if (tokenQueue.IsNotEmpty)
-        {
             throw new ParserException("Queue was not consumed till the end");
+    }
+
+    private async Task<Statement> ParseAnonymousCodeBlock(CancellationToken cancellationToken)
+    {
+        // anonymous code block: { [statement]* }
+
+        var statementsTokens = ReadUntil(TokenType.CodeBlockCloser);
+        Expect(TokenType.CodeBlockCloser);
+
+        var statements = await ParseStatements(statementsTokens, cancellationToken).ToListAsync(cancellationToken);
+
+        return new AnonymousCodeBlockStatement(statements);
+    }
+
+    private async Task<Statement> ParseWhileLoop(CancellationToken cancellationToken)
+    {
+        // looping: while (<expression>) { [statement]* }
+
+        Expect(TokenType.ExpressionOpener);
+        var expressionTokens = ReadUntil(TokenType.ExpressionCloser);
+        Expect(TokenType.ExpressionCloser);
+
+        Expect(TokenType.CodeBlockOpener);
+        var statementsTokens = ReadUntil(TokenType.CodeBlockCloser);
+        Expect(TokenType.CodeBlockCloser);
+
+        var condition = ExpressionParser.Parse(expressionTokens);
+        var statements = await ParseStatements(statementsTokens, cancellationToken).ToListAsync(cancellationToken);
+
+        return new WhileStatement(condition, statements);
+    }
+
+    private async Task<Statement> ParseIfStatement(CancellationToken cancellationToken)
+    {
+        // branching: if (<expression>) { [statement]* } [else[if (<expression>)] { [statement]* } ]
+
+        Expect(TokenType.ExpressionOpener);
+        var expressionTokens = ReadUntil(TokenType.ExpressionCloser);
+        Expect(TokenType.ExpressionCloser);
+
+        Expect(TokenType.CodeBlockOpener);
+        var trueBranchTokens = ReadUntil(TokenType.CodeBlockCloser);
+        Expect(TokenType.CodeBlockCloser);
+
+        var condition = ExpressionParser.Parse(expressionTokens);
+        var statements = await ParseStatements(trueBranchTokens, cancellationToken).ToListAsync(cancellationToken);
+
+        if (tokenQueue.PeekType() is not TokenType.ElseKeyword)
+            return new IfStatement(condition, statements);
+
+        Expect(TokenType.ElseKeyword);
+
+        IReadOnlyList<Token>? elseExpressionTokens = null;
+        if (tokenQueue.PeekType() is TokenType.IfKeyword)
+        {
+            Expect(TokenType.ExpressionOpener);
+            elseExpressionTokens = ReadUntil(TokenType.ExpressionCloser);
+            Expect(TokenType.ExpressionCloser);
         }
+
+        Expect(TokenType.CodeBlockOpener);
+        var falseBranchTokens = ReadUntil(TokenType.CodeBlockCloser);
+        Expect(TokenType.CodeBlockCloser);
+
+        var elseExpression =
+            elseExpressionTokens is not null ? ExpressionParser.Parse(elseExpressionTokens) : null;
+
+        var elseStatements = await ParseStatements(falseBranchTokens, cancellationToken)
+            .ToListAsync(cancellationToken);
+
+        return new IfElseStatement(condition, statements, elseExpression, elseStatements);
+    }
+
+    private Statement ParseIdentifierUsage(Token token)
+    {
+        var nextType = tokenQueue.PeekType();
+        if (nextType == TokenType.ExpressionOpener)
+            return ParseFunctionCall(token);
+
+        if (nextType.IsMathematicalOperation())
+            return ParseMathematicalOperation(token);
+
+        return ParseVariableAssignment(token);
+    }
+
+    private Statement ParseVariableAssignment(Token token)
+    {
+        // variable assignment: <identifier> = <expression>
+
+        Expect(TokenType.EqualsSymbol);
+        var expressionTokens = ReadUntil(TokenType.NewLine);
+        Expect(TokenType.NewLine);
+
+        var valueExpression = ExpressionParser.Parse(expressionTokens);
+
+        return new VariableAssignmentStatement(token, valueExpression);
+    }
+
+    private Statement ParseMathematicalOperation(Token token)
+    {
+        // shortcut setter: <identifier>++ / <identifier>-- / <identifier> *= <expression> / <identifier> /= <expression>
+
+        var operationToken = GetNext();
+        var valueDenominator = Expect(TokenType.PlusSymbol,
+            TokenType.MinusSymbol, TokenType.AsteriskSymbol, TokenType.SlashSymbol,
+            TokenType.PercentSymbol, TokenType.EqualsSymbol);
+
+        IReadOnlyList<Token> valueExpTokens;
+        if (valueDenominator.Type.IsMathematicalOperation())
+        {
+            // mathematical operation must be the same type as the initial operation (i.e. +- or *- is forbidden)
+            if (operationToken.Type != valueDenominator.Type)
+                throw new UnexpectedTokenException(valueDenominator, operationToken.Type);
+
+            valueExpTokens = new Token []
+            {
+                new(TokenType.LiteralNumber, "1")
+            };
+        }
+        else
+        {
+            Debug.Assert(valueDenominator.Type is TokenType.EqualsSymbol);
+
+            valueExpTokens = ReadUntil(TokenType.NewLine);
+            Expect(TokenType.NewLine);
+        }
+
+        // TODO: ugly. Move expression parsing to another class
+        var expression = ExpressionParser.Parse(new []
+        {
+            token, operationToken,
+        }.Concat(valueExpTokens).ToArray());
+
+        return new VariableAssignmentStatement(token, expression);
+    }
+
+    private Statement ParseFunctionCall(Token token)
+    {
+        // function call: <identifier>([<expression>[, <expression>]*])
+
+        Expect(TokenType.ExpressionOpener);
+        var argsTokens = ReadUntil(TokenType.ExpressionCloser);
+        Expect(TokenType.ExpressionCloser);
+
+        var args = ExpressionParser.ParseExpressions(argsTokens).ToList();
+
+        return new FunctionCallStatement(token, args);
+    }
+
+    private Statement ParseVariableDeclaration(Token token)
+    {
+        // variable declaration: <type name> <identifier> = <expression>
+
+        var identifier = Expect(TokenType.Identifier);
+        Expect(TokenType.EqualsSymbol);
+        var expressionTokens = ReadUntil(TokenType.NewLine);
+        Expect(TokenType.NewLine);
+
+        var expression = ExpressionParser.Parse(expressionTokens);
+        return new VariableDeclarationStatement(token, identifier, expression);
     }
 
     private Token GetNext(params TokenType[] allowed)
