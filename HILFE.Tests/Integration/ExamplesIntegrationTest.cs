90ff394617eb5ad0ee3cd9778eaa5c72dcbc2a45
Sören Kuklau <kuklau@trenz.de>
2023-07-23 22:13:03 +0200

    run all example `.hil` files as an integration test of sorts

---
+++ HILFE.Tests/Integration/ExamplesIntegrationTest.cs
@@ -0,0 +1,29 @@
+﻿using HILFE.Interpreting;
+using HILFE.Parsing;
+using HILFE.Tokenizing;
+
+namespace HILFE.Tests.Integration
+{
+    public class ExamplesIntegrationTest
+    {
+        [Fact]
+        public async Task TestExamplesSucceed()
+        {
+            var tokenizer = new Tokenizer();
+            var parser = new Parser();
+            var interpreter = new Interpreter(Console.Out, Console.Error, Console.In);
+
+            foreach (var hilFile in System.IO.Directory.EnumerateFiles("Examples", "*.hil"))
+            {
+                var chars = await File.ReadAllTextAsync(hilFile);
+
+                var tokens = tokenizer.TokenizeAsync(chars.ToAsyncEnumerable());
+                await parser.AddAsync(tokens);
+                var statements = parser.ParseAsync();
+                await interpreter.InterpretAsync(statements);
+
+                Assert.Equal(0, interpreter.ExitCode);
+            }
+        }
+    }
+}
