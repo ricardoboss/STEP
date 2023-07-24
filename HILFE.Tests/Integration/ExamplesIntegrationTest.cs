67272a3a1015e7d3d2b72b0223c6cc8b36e71d04
Ricardo Boss <contact@ricardoboss.de>
2023-07-24 21:45:28 +0200

    Use ClassData to load iterate example files

--- HILFE.Tests/Integration/ExamplesIntegrationTest.cs
+++ HILFE.Tests/Integration/ExamplesIntegrationTest.cs
@@ -1,29 +1,40 @@
-﻿using HILFE.Interpreting;
-using HILFE.Parsing;
+﻿using System.Collections;
+using HILFE.Interpreting;
+using HILFE.Parsing.Statements;
 using HILFE.Tokenizing;
 
-namespace HILFE.Tests.Integration
+namespace HILFE.Tests.Integration;
+
+public class ExamplesIntegrationTest
 {
-    public class ExamplesIntegrationTest
+    [Theory]
+    [ClassData(typeof(ExampleFiles))]
+    public async Task TestExamplesSucceed(string exampleFilePath)
     {
-        [Fact]
-        public async Task TestExamplesSucceed()
-        {
-            var tokenizer = new Tokenizer();
-            var parser = new Parser();
-            var interpreter = new Interpreter(Console.Out, Console.Error, Console.In);
+        var tokenizer = new Tokenizer();
+        var parser = new StatementParser();
+        var interpreter = new Interpreter(Console.Out, Console.Error, new StringReader("john"));
 
-            foreach (var hilFile in System.IO.Directory.EnumerateFiles("Examples", "*.hil"))
-            {
-                var chars = await File.ReadAllTextAsync(hilFile);
+        var chars = await File.ReadAllTextAsync(exampleFilePath);
 
-                var tokens = tokenizer.TokenizeAsync(chars.ToAsyncEnumerable());
-                await parser.AddAsync(tokens);
-                var statements = parser.ParseAsync();
-                await interpreter.InterpretAsync(statements);
+        var tokens = tokenizer.TokenizeAsync(chars.ToAsyncEnumerable());
 
-                Assert.Equal(0, interpreter.ExitCode);
-            }
-        }
+        await parser.AddAsync(tokens);
+
+        var statements = parser.ParseAsync();
+
+        await interpreter.InterpretAsync(statements);
+
+        Assert.Equal(0, interpreter.ExitCode);
+    }
+
+    public class ExampleFiles : IEnumerable<object []>
+    {
+        public IEnumerator<object []> GetEnumerator() => Directory
+            .EnumerateFiles("Examples", "*.hil")
+            .Select(path => new object[] { path })
+            .GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
