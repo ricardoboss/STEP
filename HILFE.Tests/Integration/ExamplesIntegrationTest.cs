dcc2af3e3edea684fc3e2700c16cc1209a6bb653
Ricardo Boss <mail@ricardoboss.de>
2023-07-26 11:59:00 +0200

    Added expected output given a certain input for examples

--- HILFE.Tests/Integration/ExamplesIntegrationTest.cs
+++ HILFE.Tests/Integration/ExamplesIntegrationTest.cs
@@ -11,26 +11,45 @@ public class ExamplesIntegrationTest
     [ClassData(typeof(ExampleFiles))]
     public async Task TestExamplesSucceed(string exampleFilePath)
     {
+        // arrange
+        var stdInText = "";
+        if (File.Exists(exampleFilePath + ".in"))
+            stdInText = await File.ReadAllTextAsync(exampleFilePath + ".in");
+
+        var stdOut = new StringWriter();
+        var stdErr = new StringWriter();
+        var stdIn = new StringReader(stdInText);
+
+        var expectedExitCode = 0;
+        var expectedOutput = "";
+        var expectedError = "";
+
+        if (File.Exists(exampleFilePath + ".exit"))
+            expectedExitCode = int.Parse(await File.ReadAllTextAsync(exampleFilePath + ".exit"));
+        if (File.Exists(exampleFilePath + ".out"))
+            expectedOutput = await File.ReadAllTextAsync(exampleFilePath + ".out");
+        if (File.Exists(exampleFilePath + ".err"))
+            expectedError = await File.ReadAllTextAsync(exampleFilePath + ".err");
+
         var tokenizer = new Tokenizer();
         var parser = new StatementParser();
-        var interpreter = new Interpreter(Console.Out, Console.Error, new StringReader("john"));
+        var interpreter = new Interpreter(stdOut, stdErr, stdIn);
 
+        // act
         var chars = await File.ReadAllTextAsync(exampleFilePath);
-
         tokenizer.Add(chars);
-
         var tokens = tokenizer.TokenizeAsync();
-
         await parser.AddAsync(tokens);
-
         var statements = parser.ParseAsync();
-
         await interpreter.InterpretAsync(statements);
 
-        Assert.Equal(0, interpreter.ExitCode);
+        // assert
+        Assert.Equal(expectedExitCode, interpreter.ExitCode);
+        Assert.Equal(expectedOutput, stdOut.ToString(), ignoreLineEndingDifferences: true);
+        Assert.Equal(expectedError, stdErr.ToString(), ignoreLineEndingDifferences: true);
     }
 
-    public class ExampleFiles : IEnumerable<object []>
+    private class ExampleFiles : IEnumerable<object []>
     {
         public IEnumerator<object []> GetEnumerator() => Directory
             .EnumerateFiles("Examples", "*.hil")
