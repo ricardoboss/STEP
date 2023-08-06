59bb499550580d6972ccc6fa5270d134316aa593
Ricardo Boss <contact@ricardoboss.de>
2023-08-07 01:22:19 +0200

    Update command line descriptions and removed REPL (for now)

--- STEP/Program.cs
+++ STEP/Program.cs
@@ -12,101 +12,44 @@ internal static class Program
 {
     public static async Task<int> Main(string [] args)
     {
-        var fileArgument = new Argument<FileInfo>(name: "file", description: "The path to a .hil-file")
+        var fileArgument = new Argument<FileInfo>(name: "file", description: "The path to a .step-file")
         {
             Arity = ArgumentArity.ExactlyOne,
         };
 
-        var runCommand = new Command(name: "run", description: "Run a .hil file");
+        var runCommand = new Command(name: "run", description: "Run a .step file");
         runCommand.AddArgument(fileArgument);
+        runCommand.SetHandler(Run, fileArgument);
 
-        var listenCommand = new Command(name: "listen", description: "Enter REPL mode");
-
-        var rootCommand = new RootCommand("STEPHILFE -STEP HILFE Interpreted Language For Education");
+        var rootCommand = new RootCommand("STEP - Simple Transition to Elevated Programming");
         rootCommand.AddCommand(runCommand);
-        rootCommand.AddCommand(listenCommand);
-
-        listenCommand.SetHandler(async () =>
-        {
-            var memoryBuffer = new MemoryStream();
-            var inputReader = new StreamReader(memoryBuffer);
-            var inputWriter = new StreamWriter(memoryBuffer)
-            {
-                AutoFlush = true,
-            };
-
-            var tokenizer = new Tokenizer();
-            var parser = new StatementParser();
-            var interpreter = new Interpreter(Console.Out, Console.Error, inputReader, Console.Out);
-
-            Console.WriteLine("Welcome to STEP REPL! Use Ctrl-C to exit.");
-            Console.Write("> ");
-            while (Console.ReadLine() is { } line)
-            {
-                await inputWriter.WriteLineAsync(line);
-
-                try
-                {
-                    var chars = line.Append('\n');
-
-                    tokenizer.Add(chars);
-                    var tokens = await tokenizer.TokenizeAsync().ToListAsync();
-                    if (tokens.Count == 0)
-                    {
-                        Console.Write("| ");
-
-                        continue;
-                    }
-
-                    parser.Add(tokens);
-                    var statements = await parser.ParseAsync().ToListAsync();
-                    if (statements.Count == 0)
-                    {
-                        Console.Write("| ");
-
-                        continue;
-                    }
-
-                    await interpreter.InterpretAsync(statements.ToAsyncEnumerable());
-                }
-                catch (Exception e) when (e is ParserException or TokenizerException or InterpreterException)
-                {
-                    await Console.Error.WriteLineAsync("~~> " + e.GetType().Name + ": " + e.Message);
-                }
-
-                Console.Write("> ");
-            }
-
-            await inputWriter.WriteLineAsync("exit 0");
-            Console.WriteLine("Bye!");
-        });
-
-        runCommand.SetHandler(async scriptFile =>
-        {
-            var tokenizer = new Tokenizer();
-            var parser = new StatementParser();
-            var interpreter = new Interpreter(Console.Out, Console.Error, Console.In);
-
-            var chars = await File.ReadAllTextAsync(scriptFile.FullName);
-
-            try
-            {
-                tokenizer.Add(chars);
-                var tokens = tokenizer.TokenizeAsync();
-                await parser.AddAsync(tokens);
-                var statements = parser.ParseAsync();
-                await interpreter.InterpretAsync(statements);
-
-                Environment.ExitCode = interpreter.ExitCode;
-            }
-            catch (Exception e) when (e is ParserException or TokenizerException or InterpreterException)
-            {
-                await Console.Error.WriteLineAsync("~~> " + e.GetType().Name + ": " + e.Message);
-
-                Environment.ExitCode = -1;
-            }
-        }, fileArgument);
 
         return await rootCommand.InvokeAsync(args);
+    }
+
+    private static async Task<int> Run(FileSystemInfo scriptFile)
+    {
+        var tokenizer = new Tokenizer();
+        var parser = new StatementParser();
+        var interpreter = new Interpreter(Console.Out, Console.Error, Console.In);
+
+        var chars = await File.ReadAllTextAsync(scriptFile.FullName);
+
+        try
+        {
+            tokenizer.Add(chars);
+            var tokens = tokenizer.TokenizeAsync();
+            await parser.AddAsync(tokens);
+            var statements = parser.ParseAsync();
+            await interpreter.InterpretAsync(statements);
+
+            return interpreter.ExitCode;
+        }
+        catch (Exception e) when (e is ParserException or TokenizerException or InterpreterException)
+        {
+            await Console.Error.WriteLineAsync("~~> " + e.GetType().Name + ": " + e.Message);
+
+            return -1;
+        }
     }
 }
