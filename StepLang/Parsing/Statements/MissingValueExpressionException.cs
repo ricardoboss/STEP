36b92f3bcd4c46966448926cfb57187d07f4e32b
Ricardo Boss <contact@ricardoboss.de>
2023-08-08 01:48:39 +0200

    Fix warnings

--- StepLang/Parsing/Statements/MissingValueExpressionException.cs
+++ StepLang/Parsing/Statements/MissingValueExpressionException.cs
@@ -4,7 +4,7 @@ using StepLang.Tokenizing;
 namespace StepLang.Parsing.Statements;
 
 [SuppressMessage("Design", "CA1032:Implement standard exception constructors")]
-internal class MissingValueExpressionException : ParserException
+internal sealed class MissingValueExpressionException : ParserException
 {
     public MissingValueExpressionException(Token token, Exception inner) : base(token, "A value was expected, but none was found", inner)
     {
