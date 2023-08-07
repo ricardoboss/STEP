bfaf76b7af7f714d3bf8f0b4a618bc934f5f1b85
Ricardo Boss <contact@ricardoboss.de>
2023-08-08 01:48:39 +0200

    Fix warnings

--- StepLang/Parsing/Statements/MissingConditionExpressionException.cs
+++ StepLang/Parsing/Statements/MissingConditionExpressionException.cs
@@ -4,7 +4,7 @@ using StepLang.Tokenizing;
 namespace StepLang.Parsing.Statements;
 
 [SuppressMessage("Design", "CA1032:Implement standard exception constructors")]
-internal class MissingConditionExpressionException : ParserException
+internal sealed class MissingConditionExpressionException : ParserException
 {
     public MissingConditionExpressionException(Token token, Exception inner) : base(token, "A condition was expected, but none was found", inner)
     {
