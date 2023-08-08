2577c38048c4e823ab8df3ad4fbeeea7dac383bb
Ricardo Boss <contact@ricardoboss.de>
2023-08-09 01:31:26 +0200

    Fix warnings

--- StepLang/Parsing/Statements/ImportedFileIsSelfException.cs
+++ StepLang/Parsing/Statements/ImportedFileIsSelfException.cs
@@ -4,7 +4,7 @@ using StepLang.Tokenizing;
 
 namespace StepLang.Parsing.Statements;
 
-internal class ImportedFileIsSelfException : InterpreterException
+internal sealed class ImportedFileIsSelfException : InterpreterException
 {
     public ImportedFileIsSelfException(TokenLocation location, FileSystemInfo fileInfo) : base(location, "Imported file imports itself: " + fileInfo.FullName, "Files cannot import themselves as this would cause an infinite loop. If you didn't intend to import this file, check the path of the imported file.")
     {
