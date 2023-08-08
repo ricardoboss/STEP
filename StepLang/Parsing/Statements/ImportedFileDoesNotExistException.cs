3487daaf0f81d32a494ebbf9fa2f7847de80158e
Ricardo Boss <mail@ricardoboss.de>
2023-08-08 14:51:17 +0200

    Added self-import check and disallowed import statements below first actual statement

--- StepLang/Parsing/Statements/ImportedFileDoesNotExistException.cs
+++ StepLang/Parsing/Statements/ImportedFileDoesNotExistException.cs
@@ -1,12 +1,13 @@
 using System.Diagnostics.CodeAnalysis;
 using StepLang.Interpreting;
+using StepLang.Tokenizing;
 
 namespace StepLang.Parsing.Statements;
 
 [SuppressMessage("Design", "CA1032:Implement standard exception constructors")]
-internal class ImportedFileDoesNotExistException : InterpreterException
+internal sealed class ImportedFileDoesNotExistException : InterpreterException
 {
-    public ImportedFileDoesNotExistException(FileSystemInfo fileInfo) : base("Imported file does not exist: " + fileInfo.FullName)
+    public ImportedFileDoesNotExistException(TokenLocation? importTokenLocation, FileSystemInfo fileInfo) : base(importTokenLocation, "Imported file does not exist: " + fileInfo.FullName)
     {
     }
 }
