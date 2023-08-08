855beb07afd668432c4aa8595259378e6ebe2bc4
Ricardo Boss <mail@ricardoboss.de>
2023-08-08 23:58:31 +0200

    Fix warnings

--- StepLang/Parsing/Expressions/InvalidArgumentTypeException.cs
+++ StepLang/Parsing/Expressions/InvalidArgumentTypeException.cs
@@ -3,7 +3,7 @@ using StepLang.Tokenizing;
 
 namespace StepLang.Parsing.Expressions;
 
-internal class InvalidArgumentTypeException : IncompatibleTypesException
+internal sealed class InvalidArgumentTypeException : IncompatibleTypesException
 {
     public InvalidArgumentTypeException(Token parameterTypeToken, ExpressionResult argument) : base(parameterTypeToken.Location, $"Invalid argument type. Expected {parameterTypeToken.Value}, but got {argument.ValueType}", "Make sure you're passing the correct type of argument to the function.")
     {
