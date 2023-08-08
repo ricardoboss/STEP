094a5b2967f1ea2b0b14388f160a662393255b5e
Ricardo Boss <contact@ricardoboss.de>
2023-08-09 00:45:43 +0200

    Added test for valid identifiers

--- StepLang/Tokenizing/InvalidIdentifierException.cs
+++ StepLang/Tokenizing/InvalidIdentifierException.cs
@@ -19,33 +19,9 @@ internal sealed class InvalidIdentifierException : TokenizerException
         if (char.IsDigit(value[0]))
             return generalHelp + "Yours starts with a number.";
 
-        var invalidChar = GetFirstInvalidIdentifierCharacter(value);
+        var invalidChar = value.GetFirstInvalidIdentifierCharacter();
 
-        Debug.Assert(invalidChar.HasValue);
-
-        return generalHelp + $"Yours contains an invalid character: '{invalidChar.Value}'";
-    }
-
-    private static char? GetFirstInvalidIdentifierCharacter(string value)
-    {
-        if (value.Trim().Length == 0)
-            return null;
-
-        if (value.Contains('.', StringComparison.InvariantCulture))
-            return '.';
-
-        var firstChar = value[0];
-        if (firstChar is (< 'a' or > 'z') and (< 'A' or > 'Z') and not '_')
-            return firstChar;
-
-        for (var i = 1; i < value.Length; i++)
-        {
-            var c = value[i];
-            if (c is not (>= 'a' and <= 'z' or >= 'A' and <= 'Z' or >= '0' and <= '9' or '_'))
-                return c;
-        }
-
-        return null;
+        return generalHelp + $"Yours contains an invalid character: '{invalidChar!.Value}'";
     }
 
     public InvalidIdentifierException(TokenLocation? location, string value) : base(BuildMessage(value), location, BuildHelpText(value))
--- StepLang/Tokenizing/StringExtensions.cs
+++ StepLang/Tokenizing/StringExtensions.cs
@@ -4,7 +4,23 @@ public static class StringExtensions
 {
     public static bool IsValidIdentifier(this string value)
     {
-        return value.Trim().Length > 0 && !value.Contains('.', StringComparison.InvariantCulture) &&
-               (value[0] >= 'a' && value[0] <= 'z' || value[0] >= 'A' && value[0] <= 'Z' || value[0] == '_');
+        return value.Trim().Length != 0 && !value.GetFirstInvalidIdentifierCharacter().HasValue;
+    }
+
+    public static char? GetFirstInvalidIdentifierCharacter(this string value)
+    {
+        if (value.Trim().Length == 0)
+            throw new InvalidOperationException("Cannot get the first invalid identifier character of an empty string.");
+
+        if (value.Contains('.', StringComparison.InvariantCulture))
+            return '.';
+
+        var firstChar = value[0];
+        if (!char.IsAsciiLetter(firstChar) && firstChar != '_')
+            return firstChar;
+
+        var invalidChar = value[1..].FirstOrDefault(c => !char.IsAsciiLetterOrDigit(c) && c != '_');
+
+        return invalidChar == default ? null : invalidChar;
     }
 }
