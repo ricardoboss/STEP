05f02d8164fe85dc2d7515ccdb1da8fdccb63d40
Ricardo Boss <contact@ricardoboss.de>
2023-08-09 21:20:20 +0200

    Ignore coverage of CLI project

--- StepLang.CLI/Assembly.cs
+++ StepLang.CLI/Assembly.cs
@@ -1,3 +1,4 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
+[assembly: ExcludeFromCodeCoverage]
 [assembly: SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task")]
