d8d0e333ca614bf3864991787f4a67d2c5672c6b
Ricardo Boss <contact@ricardoboss.de>
2023-08-09 02:17:01 +0200

    Move exception type to its own class

--- StepLang/Parsing/Expressions/Expression.cs
+++ StepLang/Parsing/Expressions/Expression.cs
@@ -247,10 +247,3 @@ public abstract class Expression
     [ExcludeFromCodeCoverage]
     protected virtual string DebugDisplay() => "";
 }
-
-public class IncompatibleExpressionOperandsException : IncompatibleTypesException
-{
-    public IncompatibleExpressionOperandsException(ExpressionResult a, ExpressionResult b, string operation) : base(null, $"Cannot use the {operation} operator on values of type {a.ValueType} and {b.ValueType}", "Make sure the operands are of the same type or check if the used operator can be used on the given types")
-    {
-    }
-}
