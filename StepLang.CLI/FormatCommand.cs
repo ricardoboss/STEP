1f8c3679364ca9989583ad032d44666b890de9ce
Ricardo Boss <contact@ricardoboss.de>
2023-08-11 23:13:44 +0200

    First steps (pun-intended) towards formatting command

---
+++ StepLang.CLI/FormatCommand.cs
@@ -0,0 +1,193 @@
+﻿using System.Drawing;
+using Pastel;
+using StepLang.Tokenizing;
+
+namespace StepLang.CLI;
+
+internal static class FormatCommand
+{
+    private static VerbosityWriter stdout = new(Console.Out, Verbosity.Normal);
+    private static VerbosityWriter stderr = new(Console.Error, Verbosity.Normal);
+
+    public static async Task<int> Invoke(string[] filesOrDirs, bool setExitCode, bool dryRun, Verbosity verbosity)
+    {
+        stdout = new(Console.Out, verbosity);
+        stderr = new(Console.Error, verbosity);
+
+        if (dryRun)
+            await stdout.Normal("Dry run mode enabled. No files will be modified.");
+
+        if (filesOrDirs.Length == 0)
+            filesOrDirs = new [] { "." };
+
+        var changes = 0;
+        var files = 0;
+        foreach (var s in filesOrDirs)
+        {
+            var (c, f) = await Format(s, dryRun);
+            changes += c;
+            files += f;
+        }
+
+        await stdout.Normal($"{(dryRun ? "Would have applied" : "Applied")} {changes} change(s) to {files} file(s).");
+
+        if (setExitCode)
+            return changes > 0 ? 1 : 0;
+
+        return 0;
+    }
+
+    private static async Task<(int changes, int files)> Format(string fileOrDir, bool dryRun)
+    {
+        await stdout.Normal($"Formatting '{fileOrDir.Pastel(Color.Aqua)}'...");
+
+        var changes = 0;
+        var files = 0;
+        if (File.Exists(fileOrDir))
+        {
+            changes += await FormatFile(new(fileOrDir), dryRun);
+            files++;
+        }
+        else if (Directory.Exists(fileOrDir))
+        {
+            var stepFilesEnumerable = Directory.EnumerateFiles(fileOrDir, "*.step", new EnumerationOptions
+            {
+                RecurseSubdirectories = true,
+                IgnoreInaccessible = true,
+            });
+
+            changes += await stepFilesEnumerable.ToAsyncEnumerable().AggregateAwaitAsync(
+                changes,
+                async (current, file) =>
+                {
+                    var fileChanges = await FormatFile(new(file), dryRun);
+                    if (fileChanges > 0)
+                        files++;
+                    return current + fileChanges;
+                });
+        }
+        else
+        {
+            await stderr.Normal($"The path '{fileOrDir.Pastel(Color.Aqua)}' is not a file or directory.");
+        }
+
+        return (changes, files);
+    }
+
+    private static async Task<int> FormatFile(FileInfo info, bool dryRun)
+    {
+        await stdout.Verbose("Formatting file: " + info.FullName);
+
+        List<Token> tokens;
+        try
+        {
+            var fileText = await File.ReadAllTextAsync(info.FullName);
+            var tokenizer = new Tokenizer();
+            tokenizer.UpdateFile(info);
+            tokenizer.Add(fileText);
+            tokens = tokenizer.Tokenize().ToList();
+        }
+        catch (Exception e)
+        {
+            await stderr.Quiet($"Failed to format file: {info.FullName.Pastel(Color.Aqua)}");
+            await stderr.Quiet(e.ToString());
+
+            return 0;
+        }
+
+        // var parser = new StatementParser();
+        // parser.Add(tokens);
+        // var statements = await parser.ParseAsync().ToListAsync();
+
+        await stdout.Verbose($"Found {tokens.Count} tokens.");
+
+        var changes = 0;
+        var formatted = Format(tokens, () => changes++).ToList();
+        if (!formatted.SequenceEqual(tokens))
+        {
+            if (!dryRun)
+            {
+                await File.WriteAllTextAsync(info.FullName, string.Join("", Render(formatted)));
+                await stdout.Verbose($"Wrote {changes} change(s) to file.");
+            }
+            else
+            {
+                await stdout.Verbose($"Would have written {changes} change(s) to file.");
+            }
+        }
+        else
+        {
+            await stdout.Verbose("No changes to file.");
+        }
+
+        return changes;
+    }
+
+    private static IEnumerable<string> Render(IEnumerable<Token> tokens)
+    {
+        return tokens.Select(token => token.Type switch
+        {
+            TokenType.NewLine => Environment.NewLine,
+            TokenType.LiteralString => $"\"{token.Value}\"",
+            _ => token.Value,
+        });
+    }
+
+    private static IEnumerable<Token> Format(IEnumerable<Token> tokens, Action notifyChanged)
+    {
+        using var enumerator = tokens.GetEnumerator();
+
+        enumerator.Reset();
+        if (!enumerator.MoveNext())
+            yield break;
+
+        // clear leading whitespace
+        DropLeadingWhitespace(enumerator, notifyChanged);
+
+        // order imports
+        foreach (var token in FormatImports(enumerator))
+            yield return token;
+
+        // yield the rest of the file
+        yield return enumerator.Current;
+        while (enumerator.MoveNext())
+            yield return enumerator.Current;
+    }
+
+    private static void DropLeadingWhitespace(IEnumerator<Token> enumerator, Action notifyChanged)
+    {
+        while (enumerator.Current.Type is TokenType.Whitespace or TokenType.NewLine)
+        {
+            notifyChanged();
+            if (!enumerator.MoveNext())
+                return;
+        }
+    }
+
+    private static IEnumerable<Token> FormatImports(IEnumerator<Token> enumerator)
+    {
+        var imports = new List<List<Token>>();
+        while (enumerator.Current.Type == TokenType.ImportKeyword)
+        {
+            var import = new List<Token>();
+
+            do
+            {
+                import.Add(enumerator.Current);
+            } while (enumerator.MoveNext() && !import.Any(t => t.Type is TokenType.NewLine));
+
+            imports.Add(import);
+        }
+
+        foreach (var import in imports.OrderBy(i => i[1].Value))
+        {
+            foreach (var token in import)
+                yield return token;
+
+            yield return new(TokenType.NewLine, Environment.NewLine, null);
+        }
+
+        if (imports.Count > 0 && enumerator.Current.Type != TokenType.NewLine)
+            yield return new(TokenType.NewLine, Environment.NewLine, null);
+    }
+}
--- StepLang.CLI/Program.cs
+++ StepLang.CLI/Program.cs
@@ -1,12 +1,5 @@
-using System.CommandLine;
+﻿using System.CommandLine;
 using System.Diagnostics.CodeAnalysis;
-using System.Drawing;
-using System.Globalization;
-using Pastel;
-using StepLang.Interpreting;
-using StepLang.Parsing;
-using StepLang.Parsing.Statements;
-using StepLang.Tokenizing;
 
 namespace StepLang.CLI;
 
@@ -20,106 +13,44 @@ internal static class Program
             Arity = ArgumentArity.ExactlyOne,
         };
 
+        var fileOrDirArgument = new Argument<string[]>(name: "file-or-dir", description: "The path to a .step-file or a directory of .step-files")
+        {
+            Arity = ArgumentArity.ZeroOrMore,
+        };
+
+        var setExitCodeOption = new Option<bool>(aliases: new[] { "--set-exit-code", "-s" }, description: "Set the exit code to the number of changes made")
+        {
+            IsRequired = false,
+            Arity = ArgumentArity.ZeroOrOne,
+        };
+
+        var dryRunOption = new Option<bool>(aliases: new[] { "--dry-run", "-d" }, description: "Don't actually make any changes")
+        {
+            IsRequired = false,
+            Arity = ArgumentArity.ZeroOrOne,
+        };
+
+        var verbosityOption = new Option<Verbosity>(aliases: new[] { "--verbosity", "-v" }, description: "Set the verbosity level")
+        {
+            IsRequired = false,
+            Arity = ArgumentArity.ZeroOrOne,
+        };
+
         var runCommand = new Command(name: "run", description: "Run a .step file");
         runCommand.AddArgument(fileArgument);
-        runCommand.SetHandler(Run, fileArgument);
+        runCommand.SetHandler(RunCommand.Invoke, fileArgument);
+
+        var formatCommand = new Command(name: "format", description: "Format a .step file or a directory of .step files");
+        formatCommand.AddArgument(fileOrDirArgument);
+        formatCommand.AddOption(setExitCodeOption);
+        formatCommand.AddOption(dryRunOption);
+        formatCommand.AddOption(verbosityOption);
+        formatCommand.SetHandler(FormatCommand.Invoke, fileOrDirArgument, setExitCodeOption, dryRunOption, verbosityOption);
 
         var rootCommand = new RootCommand("STEP - Simple Transition to Elevated Programming");
         rootCommand.AddCommand(runCommand);
+        rootCommand.AddCommand(formatCommand);
 
         return await rootCommand.InvokeAsync(args);
-    }
-
-    private static async Task<int> Run(FileSystemInfo scriptFile)
-    {
-        if (!scriptFile.Exists)
-        {
-            await Console.Error.WriteLineAsync(FormatError("File Not Found", $"The file '{scriptFile.FullName.Pastel(Color.Aqua)}' does not exist."));
-
-            return -1;
-        }
-
-        try
-        {
-            var chars = await File.ReadAllTextAsync(scriptFile.FullName);
-
-            var tokenizer = new Tokenizer();
-            tokenizer.UpdateFile(scriptFile);
-            tokenizer.Add(chars);
-            var tokens = tokenizer.TokenizeAsync();
-
-            var parser = new StatementParser();
-            await parser.AddAsync(tokens.ToAsyncEnumerable());
-
-            var interpreter = new Interpreter(Console.Out, Console.Error, Console.In);
-            var statements = parser.ParseAsync();
-
-            await interpreter.InterpretAsync(statements);
-
-            return interpreter.ExitCode;
-        }
-        catch (Exception e) when (e is ParserException or TokenizerException or InterpreterException or IOException)
-        {
-            await Console.Error.WriteLineAsync(FormatError(e));
-
-            return -1;
-        }
-    }
-
-    private static string FormatError(string type, string message) => ("! " + type + ": ").Pastel(Color.OrangeRed) + message;
-
-    private static string FormatError(Exception e)
-    {
-        return e switch
-        {
-            StepLangException sle => FormatStepLangException(sle),
-            _ => FormatGeneralException(e),
-        };
-    }
-
-    private static string FormatGeneralException(Exception e) => FormatError(e.GetType().Name,
-        e.Message + Environment.NewLine + e.StackTrace.Pastel(Color.DarkGray));
-
-    private static string FormatStepLangException(StepLangException e)
-    {
-        const int contextLineCount = 4;
-
-        IEnumerable<string> outputLines;
-        var exceptionName = (" " + e.GetType().Name + " ").Pastel(ConsoleColor.White).PastelBg(ConsoleColor.Red);
-        var message = Environment.NewLine + "\t" + e.Message + Environment.NewLine;
-
-        if (e.Location is { } location)
-        {
-            var sourceCode = location.File.Exists ? File.ReadAllText(location.File.FullName) : "";
-            var lines = sourceCode.ReplaceLineEndings().Split(Environment.NewLine);
-            var contextStartLine = Math.Max(0, location.Line - 1 - contextLineCount);
-            var contextEndLine = Math.Min(lines.Length, location.Line + contextLineCount);
-            var lineNumber = contextStartLine;
-            var lineNumberWidth = contextEndLine.ToString(CultureInfo.InvariantCulture).Length;
-            var contextLines = lines[contextStartLine..contextEndLine].Select(l =>
-            {
-                var prefix = lineNumber == location.Line - 1 ? $"{">".Pastel(ConsoleColor.Red)} " : "  ";
-
-                var displayLineNumber = (lineNumber + 1).ToString(CultureInfo.InvariantCulture);
-                var line = prefix + $"{(displayLineNumber.PadLeft(lineNumberWidth) + "|").Pastel(ConsoleColor.Gray)} {l}";
-
-                lineNumber++;
-
-                return line;
-            });
-
-            var locationString = $"at {location.File.FullName.Pastel(ConsoleColor.Green)}:{location.Line}";
-
-            outputLines = contextLines.Prepend(locationString);
-        }
-        else
-            outputLines = new[] { (e.StackTrace ?? string.Empty).Pastel(ConsoleColor.Gray) };
-
-        outputLines = outputLines.Prepend(message).Prepend(exceptionName);
-
-        if (e.HelpText is { } helpText)
-            outputLines = outputLines.Append(Environment.NewLine + "Tip: ".Pastel(ConsoleColor.DarkCyan) + helpText);
-
-        return Environment.NewLine + string.Join(Environment.NewLine, outputLines);
     }
 }
