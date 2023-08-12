3aaa6c14c27a3d739806a59dd5ed544525b24c0b
Sören Kuklau <kuklau@trenz.de>
2023-08-12 15:47:39 +0200

    added a `length` function, but only for strings so far

---
+++ StepLang/Framework/Pure/LengthFunction.cs
@@ -0,0 +1,29 @@
+using StepLang.Interpreting;
+using StepLang.Parsing.Expressions;
+
+namespace StepLang.Framework.IO;
+
+public class LengthFunction : NativeFunction
+{
+    public override string Identifier { get; } = "length";
+
+    public override Task<ExpressionResult> EvaluateAsync(Interpreter interpreter, IReadOnlyList<Expression> arguments,
+        CancellationToken cancellationToken = default)
+    {
+        if (arguments.Count is not 1)
+            throw new InvalidArgumentCountException(1, arguments.Count);
+
+        var exp = arguments.Single();
+        if (exp is not ConstantExpression conExp)
+        {
+            throw new InvalidExpressionTypeException(nameof(ConstantExpression), exp.GetType().Name);
+        }
+
+        var str = conExp.Result.ExpectString();
+
+        var expressionResult = new NumberResult(str.Value.Length);
+        return Task.FromResult<ExpressionResult>(expressionResult);
+    }
+
+    protected override string DebugParamsString { get; }
+}
