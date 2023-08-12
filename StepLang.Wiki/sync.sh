8cb5bfbc28874e53b896037b40862f7b797efdb5
Ricardo Boss <contact@ricardoboss.de>
2023-08-12 16:53:23 +0200

    Add more output and force with lease

--- StepLang.Wiki/sync.sh
+++ StepLang.Wiki/sync.sh
@@ -10,11 +10,14 @@ git config user.name "$USER_NAME"
 git config user.email "$USER_EMAIL"
 git remote add wiki "https://$USER_NAME:$USER_TOKEN@github.com/$REPOSITORY_NAME.wiki.git"
 
-# if there are no change, just exit
+# if there are no changes, just exit
+echo "Checking for changes..."
 git status --porcelain && exit 0
 
+# commit and push
+echo "Committing and pushing..."
 git add .
 git commit --verbose -m "$COMMIT_MSG"
-git push -f --set-upstream wiki master --verbose
+git push --all --force-with-lease --set-upstream wiki master --verbose
 
 popd
