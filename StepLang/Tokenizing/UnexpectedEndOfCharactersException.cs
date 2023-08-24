468c7b2ece0ed1c6594d1f58daa0c73a9c6acc1a
Ricardo Boss <mail@ricardoboss.de>
2023-08-24 15:51:04 +0200

    Removed possible exception and added description for TOK001

--- StepLang/Tokenizing/UnexpectedEndOfCharactersException.cs
+++
@@ -1,8 +0,0 @@
-namespace StepLang.Tokenizing;
-
-internal sealed class UnexpectedEndOfCharactersException : TokenizerException
-{
-    public UnexpectedEndOfCharactersException(TokenLocation? location) : base(1, location, "Unexpected end of character input", "The current token was not complete. Check your spelling, parentheses, and other punctuation.")
-    {
-    }
-}
