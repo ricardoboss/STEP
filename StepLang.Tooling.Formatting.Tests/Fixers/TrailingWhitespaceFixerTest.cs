8768946af5521868708b6d1b956a951fc83379e0
Ricardo Boss <mail@ricardoboss.de>
2023-08-25 23:16:54 +0200

    Added some tests with tab at the end of lines

--- StepLang.Tooling.Formatting.Tests/Fixers/TrailingWhitespaceFixerTest.cs
+++ StepLang.Tooling.Formatting.Tests/Fixers/TrailingWhitespaceFixerTest.cs
@@ -9,12 +9,17 @@ public class TrailingWhitespaceFixerTest
     [InlineData("a ", "a")]
     [InlineData(" a", " a")]
     [InlineData(" a ", " a")]
+    [InlineData("a\t", "a")]
+    [InlineData("\ta", "\ta")]
+    [InlineData("\ta\t", "\ta")]
+    [InlineData("a\n\t", "a\n")]
     [InlineData("a\n", "a\n")]
     [InlineData("a\r\n", "a\r\n")]
     [InlineData("a\r\n ", "a\r\n")]
     [InlineData("a\r\n  ", "a\r\n")]
     [InlineData("a\n b\n ", "a\n b\n")]
     [InlineData("a \n b\n", "a\n b\n")]
+    [InlineData("a\t\n\tb\n", "a\n\tb\n")]
     public async Task TestApplyRemovesTrailingWhitespace(string input, string output)
     {
         var fixer = new TrailingWhitespaceFixer();
