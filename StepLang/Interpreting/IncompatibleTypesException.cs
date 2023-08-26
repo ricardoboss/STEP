5fcc07fc9dd783d18c6131be63a78bfadb8f51fe
Ricardo Boss <mail@ricardoboss.de>
2023-08-26 17:04:45 +0200

    Moved incompatible types exceptions to their own prefix (TYP) to prevent duplicate IDs

--- StepLang/Interpreting/IncompatibleTypesException.cs
+++ StepLang/Interpreting/IncompatibleTypesException.cs
@@ -4,7 +4,7 @@ namespace StepLang.Interpreting;
 
 public abstract class IncompatibleTypesException : InterpreterException
 {
-    protected IncompatibleTypesException(int errorCode, TokenLocation? location, string message, string helpText, Exception? inner = null) : base(errorCode, location, message, helpText, inner)
+    protected IncompatibleTypesException(int errorCode, TokenLocation? location, string message, string helpText, Exception? inner = null) : base($"TYP{errorCode:000}", location, message, helpText, inner)
     {
     }
 }
--- StepLang/Parsing/Expressions/IncompatibleExpressionOperandsException.cs
+++ StepLang/Parsing/Expressions/IncompatibleExpressionOperandsException.cs
@@ -4,7 +4,7 @@ namespace StepLang.Parsing.Expressions;
 
 public class IncompatibleExpressionOperandsException : IncompatibleTypesException
 {
-    public IncompatibleExpressionOperandsException(ExpressionResult a, ExpressionResult b, string operation) : base(8, null, $"Cannot use the {operation} operator on values of type {a.ResultType.ToTypeName()} and {b.ResultType.ToTypeName()}", "Make sure the operands are of the same type or check if the used operator can be used on the given types")
+    public IncompatibleExpressionOperandsException(ExpressionResult a, ExpressionResult b, string operation) : base(3, null, $"Cannot use the {operation} operator on values of type {a.ResultType.ToTypeName()} and {b.ResultType.ToTypeName()}", "Make sure the operands are of the same type or check if the used operator can be used on the given types")
     {
     }
 }
