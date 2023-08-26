b276ba6da6e67e23e6f5b8b2ae1cdb012e9fffa9
Ricardo Boss <mail@ricardoboss.de>
2023-08-26 14:33:03 +0200

    Added remaining exception descriptions

--- StepLang/Interpreting/IncompatibleVariableTypeException.cs
+++ StepLang/Interpreting/IncompatibleVariableTypeException.cs
@@ -4,7 +4,7 @@ namespace StepLang.Interpreting;
 
 public class IncompatibleVariableTypeException : IncompatibleTypesException
 {
-    public IncompatibleVariableTypeException(Variable variable, ExpressionResult newValue) : base(3, null, $"Cannot assign a value of a type {newValue.ResultType.ToTypeName()} to a variable declared as {variable.Value.ResultType.ToTypeName()}", $"Make sure the value you are trying to assign to {variable.Identifier} is of type {variable.Value.ResultType.ToTypeName()}.")
+    public IncompatibleVariableTypeException(Variable variable, ExpressionResult newValue) : base(7, null, $"Cannot assign a value of a type {newValue.ResultType.ToTypeName()} to a variable declared as {variable.Value.ResultType.ToTypeName()}", $"Make sure the value you are trying to assign to {variable.Identifier} is of type {variable.Value.ResultType.ToTypeName()}.")
     {
     }
 }
--- StepLang/Parsing/Statements/InvalidVariableAssignmentException.cs
+++ StepLang/Parsing/Statements/InvalidVariableAssignmentException.cs
@@ -5,7 +5,7 @@ namespace StepLang.Parsing.Statements;
 
 public class InvalidVariableAssignmentException : IncompatibleTypesException
 {
-    public InvalidVariableAssignmentException(Token identifierToken, StepLangException inner) : base(2, identifierToken.Location, inner.Message, inner.HelpText, inner)
+    public InvalidVariableAssignmentException(Token identifierToken, StepLangException inner) : base(6, identifierToken.Location, inner.Message, inner.HelpText, inner)
     {
     }
 }
