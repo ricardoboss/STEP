68e712a03b233c14ae2ffcb72dfe6ac50120c8e0
Ricardo Boss <mail@ricardoboss.de>
2023-08-28 01:23:28 +0200

    Added more tests and fixed documentation for PAR004 and PAR007

--- StepLang.Tests/Parsing/ParserTest.cs
+++ StepLang.Tests/Parsing/ParserTest.cs
@@ -1,6 +1,7 @@
+using StepLang.Interpreting;
 using StepLang.Parsing;
+using StepLang.Parsing.Expressions;
 using StepLang.Parsing.Statements;
-using StepLang.Tokenizing;
 
 namespace StepLang.Tests.Parsing;
 
@@ -9,13 +10,7 @@ public class ParserTest
     [Fact]
     public async Task TestParseFunctionCallNoParams()
     {
-        var parser = new StatementParser();
-        parser.Add(new Token[]
-        {
-            new(TokenType.Identifier, "function", null),
-            new(TokenType.OpeningParentheses, "(", null),
-            new(TokenType.ClosingParentheses, ")", null),
-        });
+        var parser = "func()".AsParsable();
 
         var statements = await parser.ParseAsync().ToListAsync();
 
@@ -26,13 +21,80 @@ public class ParserTest
     [Fact]
     public async Task TestParseUnexpectedTokenThrows()
     {
-        var parser = new StatementParser();
-        parser.Add(new Token[]
-        {
-            new(TokenType.LiteralString, "string", null),
-        });
+        var parser = "\"string\"".AsParsable();
 
-        await Assert.ThrowsAsync<UnexpectedTokenException>(async () => await parser.ParseAsync().ToListAsync());
+        var exception = await Assert.ThrowsAsync<UnexpectedTokenException>(async () => await parser.ParseAsync().ToListAsync());
+
+        Assert.Equal("PAR001", exception.ErrorCode);
+    }
+
+    [Fact]
+    public async Task TestMissingValueThrows()
+    {
+        var parser = "number a = ".AsParsable();
+
+        var exception = await Assert.ThrowsAsync<MissingValueExpressionException>(async () => await parser.ParseAsync().ToListAsync());
+
+        Assert.Equal("PAR003", exception.ErrorCode);
+    }
+
+    [Fact]
+    public async Task TestMissingConditionThrows()
+    {
+        var parser = """
+                     if () {
+                         print("Hello World!")
+                     }
+                     """.AsParsable();
+
+        var exception = await Assert.ThrowsAsync<MissingConditionExpressionException>(async () => await parser.ParseAsync().ToListAsync());
+
+        Assert.Equal("PAR004", exception.ErrorCode);
+    }
+
+    [Fact]
+    public async Task TestImportedFileDoesNotExistThrows()
+    {
+        var statement = await "import \"doesnotexist.step\"".AsStatementAsync();
+        var interpreter = new Interpreter();
+
+        var exception = await Assert.ThrowsAsync<ImportedFileDoesNotExistException>(async () => await statement.ExecuteAsync(interpreter));
+
+        Assert.Equal("PAR005", exception.ErrorCode);
+    }
+
+    [Fact]
+    public async Task TestImportStatementNoLongerAllowedThrows()
+    {
+        var parser = """
+                     // this is a comment
+                     import "somefile.step"
+
+                     number x = 1
+
+                     import "someotherfile.step"
+                     """.AsParsable();
+
+        var exception = await Assert.ThrowsAsync<ImportsNoLongerAllowedException>(async () => await parser.ParseAsync().ToListAsync());
+
+        Assert.Equal("PAR007", exception.ErrorCode);
+    }
+
+    [Fact]
+    public async Task TestInvalidIndexOperatorThrows()
+    {
+        var statements = await """
+                               number a = 1
+
+                               println(a[0])
+                               """.AsStatementsAsync();
+
+        var output = new StringWriter();
+        var interpreter = new Interpreter(output);
+
+        var exception = await Assert.ThrowsAsync<InvalidIndexOperatorException>(async () => await interpreter.InterpretAsync(statements.ToAsyncEnumerable()));
+
+        Assert.Equal("PAR009", exception.ErrorCode);
     }
 
     // [Fact]
