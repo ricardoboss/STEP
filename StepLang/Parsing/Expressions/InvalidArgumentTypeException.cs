ace82e6af580085fa66aa762dd390e8ff1c0fff5
Ricardo Boss <contact@ricardoboss.de>
2023-09-04 20:13:56 +0200

    Added ability to throw InvalidArgumentTypeException without a token

--- StepLang/Parsing/Expressions/InvalidArgumentTypeException.cs
+++ StepLang/Parsing/Expressions/InvalidArgumentTypeException.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+using System.Text;
 using StepLang.Interpreting;
 using StepLang.Tokenizing;
 
@@ -5,7 +7,27 @@ namespace StepLang.Parsing.Expressions;
 
 internal sealed class InvalidArgumentTypeException : IncompatibleTypesException
 {
-    public InvalidArgumentTypeException(Token parameterTypeToken, ExpressionResult argument) : base(3, parameterTypeToken.Location, $"Invalid argument type. Expected {parameterTypeToken.Value}, but got {argument.ResultType.ToTypeName()}", "Make sure you're passing the correct type of argument to the function.")
+    private static string BuildMessage(IReadOnlyList<ResultType> acceptedTypes, ResultType gotType)
+    {
+        var sb = new StringBuilder("Invalid argument type. Expected ");
+
+        sb.Append(acceptedTypes.Count switch
+        {
+            0 => "no arguments",
+            1 => $"{acceptedTypes[0].ToTypeName()}",
+            _ => $"{string.Join(", ", acceptedTypes.Take(acceptedTypes.Count - 1).Select(t => t.ToTypeName()))} or {acceptedTypes[^1].ToTypeName()}",
+        });
+
+        sb.Append(CultureInfo.InvariantCulture, $", but got {gotType.ToTypeName()}");
+
+        return sb.ToString();
+    }
+
+    public InvalidArgumentTypeException(Token parameterTypeToken, ExpressionResult argument) : this(parameterTypeToken.Location, new[] { ValueTypeExtensions.FromTypeName(parameterTypeToken.Value) }, argument)
+    {
+    }
+
+    public InvalidArgumentTypeException(TokenLocation? location, IReadOnlyList<ResultType> acceptedParameterTypes, ExpressionResult argument) : base(3, location, BuildMessage(acceptedParameterTypes, argument.ResultType), "Make sure you're passing the correct type of argument to the function.")
     {
     }
 }
