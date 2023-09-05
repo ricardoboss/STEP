68f4460832704dc87492be895f1b40557c588b22
Ricardo Boss <contact@ricardoboss.de>
2023-09-05 13:23:31 +0200

    Added reversed function

---
+++ StepLang.Wiki/Functions/Reversed.md
@@ -0,0 +1,21 @@
+# Description
+
+`reversed` returns a new list with the elements in reverse order.
+
+# Syntax
+
+```step
+reversed(list subject)
+```
+
+- `subject` is the list to reverse.
+
+# Remarks
+
+- `reversed` is a pure function and doesn't modify the original list.
+
+# Examples
+
+```step
+reversed([1, 2, 3]) // [3, 2, 1]
+```
---
+++ StepLang/Framework/Pure/ReversedFunction.cs
@@ -0,0 +1,20 @@
+using StepLang.Interpreting;
+using StepLang.Parsing.Expressions;
+
+namespace StepLang.Framework.Pure;
+
+public class ReversedFunction : NativeFunction
+{
+    public const string Identifier = "reversed";
+
+    public override IEnumerable<(ResultType[] types, string identifier)> Parameters => new[] { (new[] { ResultType.List }, "subject") };
+
+    public override async Task<ExpressionResult> EvaluateAsync(Interpreter interpreter, IReadOnlyList<Expression> arguments, CancellationToken cancellationToken = default)
+    {
+        CheckArgumentCount(arguments);
+
+        var subjectResult = await arguments.Single().EvaluateAsync(interpreter, r => r.ExpectList(), cancellationToken);
+        var reversed = subjectResult.DeepClone().Value.Reverse().ToList();
+        return new ListResult(reversed);
+    }
+}
