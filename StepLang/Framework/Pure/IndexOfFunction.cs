d52274c18f2f5879fdabcbd4f0451084ccced44d
Ricardo Boss <contact@ricardoboss.de>
2023-09-05 10:10:48 +0200

    Added `indexOf` function
    This change introduces a new function `indexOf` function which provides the index of the first occurrence of a substring in a string, the index of an element in a list, or the key of the first occurrence of a value in a map.

---
+++ StepLang/Framework/Pure/IndexOfFunction.cs
@@ -0,0 +1,39 @@
+using StepLang.Interpreting;
+using StepLang.Parsing.Expressions;
+
+namespace StepLang.Framework.Pure;
+
+public class IndexOfFunction : NativeFunction
+{
+    public const string Identifier = "indexOf";
+
+    public override async Task<ExpressionResult> EvaluateAsync(Interpreter interpreter,
+        IReadOnlyList<Expression> arguments, CancellationToken cancellationToken = default)
+    {
+        CheckArgumentCount(arguments, 2);
+
+        var (subjectExpression, valueExpression) = (arguments[0], arguments[1]);
+
+        var subject = await subjectExpression.EvaluateAsync(interpreter, cancellationToken);
+        var value = await valueExpression.EvaluateAsync(interpreter, cancellationToken);
+
+        return subject switch
+        {
+            ListResult list => new NumberResult(list.Value.IndexOf(value)),
+            MapResult map => GetMapKey(map, value),
+            StringResult str => new NumberResult(str.Value.IndexOf(value.ExpectString().Value, StringComparison.Ordinal)),
+            _ => NullResult.Instance,
+        };
+    }
+
+    private static ExpressionResult GetMapKey(MapResult map, ExpressionResult value)
+    {
+        var pair = map.Value.FirstOrDefault(x => x.Value.Equals(value));
+
+        return pair.Equals(default(KeyValuePair<string, ExpressionResult>)) ?
+            NullResult.Instance :
+            new StringResult(pair.Key);
+    }
+
+    protected override string DebugParamsString => "list|map|string subject, any|string value";
+}
