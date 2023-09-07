55e4dfabbd0351121ddb673cea7c646f64a131fb
Ricardo Boss <contact@ricardoboss.de>
2023-09-07 20:41:10 +0200

    Added ability to pass expression results to InvalidResultTypeException for added context in message

--- StepLang/Interpreting/InvalidResultTypeException.cs
+++ StepLang/Interpreting/InvalidResultTypeException.cs
@@ -4,19 +4,44 @@ namespace StepLang.Interpreting;
 
 public class InvalidResultTypeException : IncompatibleTypesException
 {
-    private static string BuildMessage(ResultType got, ResultType[] expected)
+    private static string ToExpectation(IReadOnlyList<ResultType> expected) => expected.Count switch
     {
-        var expectation = expected.Length switch
-        {
-            0 => "nothing",
-            1 => $"a {expected[0].ToTypeName()}",
-            _ => $"one of {string.Join(", ", expected.Select(e => e.ToTypeName()))}",
-        };
+        0 => "nothing",
+        1 => $"a {expected[0].ToTypeName()}",
+        _ => $"one of {string.Join(", ", expected.Select(e => e.ToTypeName()))}",
+    };
+
+    private static string BuildMessage(ResultType got, IReadOnlyList<ResultType> expected)
+    {
+        var expectation = ToExpectation(expected);
 
         return $"Invalid result type: expected {expectation}, got {got.ToTypeName()}";
     }
 
-    public InvalidResultTypeException(ResultType got, params ResultType[] expected) : base(4, null, BuildMessage(got, expected), "An expression evaluated to an unexpected type. Check what types of expression results are allowed in the current context.")
+    private static string BuildMessage(ExpressionResult got, IReadOnlyList<ResultType> expected)
+    {
+        var expectation = ToExpectation(expected);
+        var gotString = got.ResultType switch
+        {
+            ResultType.Void => "void",
+            ResultType.Null => "null",
+            ResultType.Function => "a function",
+            ResultType.Str => $"a string (\"{got}\")",
+            _ => $"a {got.ResultType.ToTypeName()} ({got})",
+        };
+
+        return $"Invalid result type: expected {expectation}, got {gotString}";
+    }
+
+    public InvalidResultTypeException(ResultType got, params ResultType[] expected) : this(BuildMessage(got, expected))
+    {
+    }
+
+    public InvalidResultTypeException(ExpressionResult got, params ResultType[] expected) : this(BuildMessage(got, expected))
+    {
+    }
+
+    private InvalidResultTypeException(string message) : base(4, null, message, "An expression evaluated to an unexpected type. Check what types of expression results are allowed in the current context.")
     {
     }
 }
