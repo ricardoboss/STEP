06201380abb8e86ea3aa129002befacbbbda38d8
Ricardo Boss <contact@ricardoboss.de>
2023-09-07 20:42:28 +0200

    Added compareTo and sorted functions

---
+++ StepLang/Framework/Pure/CompareToFunction.cs
@@ -0,0 +1,37 @@
+using StepLang.Interpreting;
+using StepLang.Parsing.Expressions;
+
+namespace StepLang.Framework.Pure;
+
+public class CompareToFunction : NativeFunction
+{
+    public const string Identifier = "compareTo";
+
+    public override IEnumerable<(ResultType[] types, string identifier)> Parameters => new[] { (Enum.GetValues<ResultType>(), "a"), (Enum.GetValues<ResultType>(), "b") };
+
+    public override async Task<ExpressionResult> EvaluateAsync(Interpreter interpreter, IReadOnlyList<Expression> arguments, CancellationToken cancellationToken = default)
+    {
+        CheckArgumentCount(arguments);
+
+        var (aExpression, bExpression) = (arguments[0], arguments[1]);
+
+        var a = await aExpression.EvaluateAsync(interpreter, cancellationToken);
+        var b = await bExpression.EvaluateAsync(interpreter, cancellationToken);
+
+        var comparison = a switch
+        {
+            StringResult aStr when b is StringResult bStr => aStr.CompareTo(bStr),
+            NumberResult aNum when b is NumberResult bNum => aNum.CompareTo(bNum),
+            BoolResult aBool when b is BoolResult bBool => aBool.Value.CompareTo(bBool.Value),
+            ListResult aList when b is ListResult bList => aList.Value.Count.CompareTo(bList.Value.Count),
+            MapResult aMap when b is MapResult bMap => aMap.Value.Count.CompareTo(bMap.Value.Count),
+            FunctionResult when b is FunctionResult => 0,
+            NullResult when b is NullResult => 0,
+            VoidResult when b is VoidResult => 0,
+            { ResultType: var aType } when b.ResultType != aType => throw new InvalidResultTypeException(b, a.ResultType),
+            _ => throw new NotImplementedException(),
+        };
+
+        return new NumberResult(comparison);
+    }
+}
---
+++ StepLang/Framework/Pure/SortedFunction.cs
@@ -0,0 +1,57 @@
+using System.Runtime.CompilerServices;
+using StepLang.Interpreting;
+using StepLang.Parsing.Expressions;
+
+namespace StepLang.Framework.Pure;
+
+public class SortedFunction : ListManipulationFunction
+{
+    public const string Identifier = "sorted";
+
+    public override async Task<ExpressionResult> EvaluateAsync(Interpreter interpreter, IReadOnlyList<Expression> arguments, CancellationToken cancellationToken = default)
+    {
+        CheckArgumentCount(arguments, 1, 2);
+
+        if (arguments.Count == 2)
+            return await base.EvaluateAsync(interpreter, arguments, cancellationToken);
+
+        // use compareTo defined in the current scope and fall back to the default implementation
+        ExpressionResult callback;
+        if (interpreter.CurrentScope.TryGetVariable(CompareToFunction.Identifier, out var compareToVariable))
+            callback = compareToVariable.Value;
+        else
+            callback = new CompareToFunction().ToResult();
+
+        return await base.EvaluateAsync(interpreter, new[] { arguments[0], callback.ToExpression() }, cancellationToken);
+    }
+
+    protected override IAsyncEnumerable<ConstantExpression[]> PrepareArgsForCallback(IEnumerable<ExpressionResult> list, FunctionDefinition callback)
+    {
+        var callbackParameters = callback.Parameters.ToList();
+        if (callbackParameters.Count != 2)
+            throw new InvalidArgumentTypeException(null, $"Callback function must have 2 parameters, but has {callbackParameters.Count}");
+
+        if (!callbackParameters[0].types.SequenceEqual(callbackParameters[1].types))
+            throw new InvalidArgumentTypeException(null, $"Both parameters of callback function must have the same type, but are {string.Join("|", callbackParameters[0].types.Select(t => t.ToTypeName()))} and {string.Join("|", callbackParameters[1].types.Select(t => t.ToTypeName()))}");
+
+        return list.Select(e => new[] { e.ToExpression() }).ToAsyncEnumerable();
+    }
+
+    protected override async IAsyncEnumerable<ExpressionResult> EvaluateListManipulationAsync(Interpreter interpreter, IAsyncEnumerable<ConstantExpression[]> arguments,
+        FunctionDefinition callback, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        var arr = await arguments.ToArrayAsync(cancellationToken);
+
+        Array.Sort(arr, (a, b) =>
+        {
+            var args = new[] { a[0], b[0] };
+
+            var result = callback.EvaluateAsync(interpreter, args, cancellationToken).Result.ExpectNumber().Value;
+
+            return Math.Sign(result);
+        });
+
+        foreach (var arg in arr)
+            yield return arg[0].Result;
+    }
+}
