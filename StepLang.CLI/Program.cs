bdd1c72c7b58745782e7dc9b952ad1bd94c2d3dc
Ricardo Boss <contact@ricardoboss.de>
2023-09-11 20:08:58 +0200

    Removed faulty assembly version, added --info and custom --version option handling, revised exception handling

--- StepLang.CLI/Program.cs
+++ StepLang.CLI/Program.cs
@@ -1,11 +1,18 @@
 using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Invocation;
+using System.CommandLine.IO;
+using System.CommandLine.Parsing;
 using System.Diagnostics.CodeAnalysis;
+using cmdwtf;
 
 namespace StepLang.CLI;
 
 [ExcludeFromCodeCoverage]
 internal static class Program
 {
+    private const string Slogan = "STEP - Simple Transition to Elevated Programming";
+
     public static async Task<int> Main(string[] args)
     {
         var fileArgument = new Argument<FileInfo>(name: "file", description: "The path to a .step-file")
@@ -42,6 +49,18 @@ internal static class Program
             Arity = ArgumentArity.ZeroOrOne,
         };
 
+        var versionOption = new Option<bool>(aliases: new[] { "--version" }, description: "Print the version number")
+        {
+            IsRequired = false,
+            Arity = ArgumentArity.ZeroOrOne,
+        };
+
+        var infoOption = new Option<bool>(aliases: new[] { "--info" }, description: "Print the version number and other information")
+        {
+            IsRequired = false,
+            Arity = ArgumentArity.ZeroOrOne,
+        };
+
         var runCommand = new Command(name: "run", description: "Run a .step file");
         runCommand.AddArgument(fileArgument);
         runCommand.SetHandler(RunCommand.Invoke, fileArgument);
@@ -53,13 +72,62 @@ internal static class Program
         formatCommand.AddOption(verbosityOption);
         formatCommand.SetHandler(FormatCommand.Invoke, fileOrDirArgument, setExitCodeOption, dryRunOption, verbosityOption);
 
-        var rootCommand = new RootCommand("STEP - Simple Transition to Elevated Programming");
+        var rootCommand = new RootCommand(Slogan);
         rootCommand.AddArgument(hiddenFileArgument);
+        rootCommand.AddOption(versionOption);
+        rootCommand.AddOption(infoOption);
         rootCommand.SetHandler(RunCommand.Invoke, hiddenFileArgument);
 
         rootCommand.AddCommand(runCommand);
         rootCommand.AddCommand(formatCommand);
 
-        return await rootCommand.InvokeAsync(args);
+        var builder = new CommandLineBuilder(rootCommand)
+            .UseHelp()
+            .UseEnvironmentVariableDirective()
+            .UseSuggestDirective()
+            .RegisterWithDotnetSuggest()
+            .UseTypoCorrections()
+            .UseParseDirective(-3)
+            .UseParseErrorReporting(-2)
+            .UseExceptionHandler(ErrorHandler.HandleException)
+            .CancelOnProcessTermination();
+
+        builder.AddMiddleware(async (context, next) =>
+        {
+            if (context.ParseResult.FindResultFor(versionOption) is not null)
+            {
+                HandleVersionOption(context);
+
+                return;
+            }
+
+            if (context.ParseResult.FindResultFor(infoOption) is not null)
+            {
+                HandleInfoOption(context);
+
+                return;
+            }
+
+            await next(context);
+        }, MiddlewareOrder.Configuration);
+
+        return await builder.Build().InvokeAsync(args);
+    }
+
+    private static void HandleVersionOption(InvocationContext context)
+    {
+        context.Console.Out.WriteLine(GitVersionInformation.FullSemVer);
+    }
+
+    private static void HandleInfoOption(InvocationContext context)
+    {
+        context.Console.Out.WriteLine($"""
+                                   {Slogan}
+                                   Build Date:  {BuildTimestamp.BuildTimeUtc:yyyy-MM-dd HH:mm:ss} UTC
+                                   Version:     {GitVersionInformation.Sha} ({GitVersionInformation.FullSemVer})
+                                   Branch:      {GitVersionInformation.BranchName}
+                                   CLR Version: {Environment.Version}
+                                   OS Version:  {Environment.OSVersion}
+                                   """);
     }
 }
--- StepLang.CLI/RunCommand.cs
+++ StepLang.CLI/RunCommand.cs
@@ -1,8 +1,5 @@
-using System.Drawing;
-using System.Globalization;
-using Pastel;
+using System.Diagnostics;
 using StepLang.Interpreting;
-using StepLang.Parsing;
 using StepLang.Parsing.Statements;
 using StepLang.Tokenizing;
 
@@ -12,101 +9,23 @@ internal static class RunCommand
 {
     public static async Task<int> Invoke(FileSystemInfo scriptFile)
     {
-        if (!scriptFile.Exists)
-        {
-            await Console.Error.WriteLineAsync(FormatError("File Not Found", $"The file '{scriptFile.FullName.Pastel(Color.Aqua)}' does not exist."));
+        Debug.Assert(scriptFile.Exists);
 
-            return -1;
-        }
+        var chars = await File.ReadAllTextAsync(scriptFile.FullName);
 
-        try
-        {
-            var chars = await File.ReadAllTextAsync(scriptFile.FullName);
+        var tokenizer = new Tokenizer();
+        tokenizer.UpdateFile(scriptFile);
+        tokenizer.Add(chars);
+        var tokens = tokenizer.Tokenize();
 
-            var tokenizer = new Tokenizer();
-            tokenizer.UpdateFile(scriptFile);
-            tokenizer.Add(chars);
-            var tokens = tokenizer.Tokenize();
+        var parser = new StatementParser();
+        await parser.AddAsync(tokens.ToAsyncEnumerable());
 
-            var parser = new StatementParser();
-            await parser.AddAsync(tokens.ToAsyncEnumerable());
+        var interpreter = new Interpreter(Console.Out, Console.Error, Console.In);
+        var statements = parser.ParseAsync();
 
-            var interpreter = new Interpreter(Console.Out, Console.Error, Console.In);
-            var statements = parser.ParseAsync();
+        await interpreter.InterpretAsync(statements);
 
-            await interpreter.InterpretAsync(statements);
-
-            return interpreter.ExitCode;
-        }
-        catch (Exception e) when (e is ParserException or TokenizerException or InterpreterException or IOException)
-        {
-            await Console.Error.WriteLineAsync(FormatError(e));
-
-            return -1;
-        }
-    }
-
-    private static string FormatError(string type, string message) => ("! " + type + ": ").Pastel(Color.OrangeRed) + message;
-
-    private static string FormatError(Exception e)
-    {
-        return e switch
-        {
-            StepLangException sle => FormatStepLangException(sle),
-            _ => FormatGeneralException(e),
-        };
-    }
-
-    private static string FormatGeneralException(Exception e) => FormatError(e.GetType().Name,
-        e.Message + Environment.NewLine + e.StackTrace.Pastel(Color.DarkGray));
-
-    private static string FormatStepLangException(StepLangException e)
-    {
-        const int contextLineCount = 4;
-
-        IEnumerable<string> outputLines;
-
-        var exceptionName = (" " + e.ErrorCode + ": " + e.GetType().Name + " ")
-            .Pastel(ConsoleColor.White)
-            .PastelBg(ConsoleColor.Red);
-
-        var message = Environment.NewLine + "\t" + e.Message + Environment.NewLine;
-
-        if (e.Location is { } location)
-        {
-            var sourceCode = location.File.Exists ? File.ReadAllText(location.File.FullName) : "";
-            var lines = sourceCode.ReplaceLineEndings().Split(Environment.NewLine);
-            var contextStartLine = Math.Max(0, location.Line - 1 - contextLineCount);
-            var contextEndLine = Math.Min(lines.Length, location.Line + contextLineCount);
-            var lineNumber = contextStartLine;
-            var lineNumberWidth = contextEndLine.ToString(CultureInfo.InvariantCulture).Length;
-            var contextLines = lines[contextStartLine..contextEndLine].Select(l =>
-            {
-                var prefix = lineNumber == location.Line - 1 ? $"{">".Pastel(ConsoleColor.Red)} " : "  ";
-
-                var displayLineNumber = (lineNumber + 1).ToString(CultureInfo.InvariantCulture);
-                var line = prefix + $"{(displayLineNumber.PadLeft(lineNumberWidth) + "|").Pastel(ConsoleColor.Gray)} {l}";
-
-                lineNumber++;
-
-                return line;
-            });
-
-            var locationString = $"at {location.File.FullName.Pastel(ConsoleColor.Green)}:{location.Line}";
-
-            outputLines = contextLines.Prepend(locationString);
-        }
-        else
-            outputLines = new[] { (e.StackTrace ?? string.Empty).Pastel(ConsoleColor.Gray) };
-
-        outputLines = outputLines.Prepend(message).Prepend(exceptionName);
-
-        if (e.HelpText is { } helpText)
-            outputLines = outputLines.Append(Environment.NewLine + "Tip: ".Pastel(ConsoleColor.DarkCyan) + helpText);
-
-        if (e.HelpLink is { } helpLink)
-            outputLines = outputLines.Append(Environment.NewLine + "See also: ".Pastel(ConsoleColor.DarkCyan) + helpLink);
-
-        return Environment.NewLine + string.Join(Environment.NewLine, outputLines);
+        return interpreter.ExitCode;
     }
 }
