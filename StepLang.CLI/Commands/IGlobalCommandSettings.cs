86226963e71c749179a27aa5289ba136080d35e1
Ricardo Boss <contact@ricardoboss.de>
2023-09-25 00:58:51 +0200

    Add -v alias for version

--- StepLang.CLI/Commands/IGlobalCommandSettings.cs
+++ StepLang.CLI/Commands/IGlobalCommandSettings.cs
@@ -5,7 +5,7 @@ internal interface IGlobalCommandSettings
     public const string InfoOptionName = "--info";
     public const string InfoOptionDescription = "Print the version and system information. Add the output of this to bug reports.";
     public const bool InfoOptionDefaultValue = false;
-    public const string VersionOptionName = "--version";
+    public const string VersionOptionName = "-v|--version";
     public const string VersionOptionDescription = "Print the version number.";
     public const bool VersionOptionDefaultValue = false;
 
