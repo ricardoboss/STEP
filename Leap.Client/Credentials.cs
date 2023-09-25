5358ca7e5ca79d911b9415b24936b8464d2d6dbd
Ricardo Boss <mail@ricardoboss.de>
2023-09-25 11:03:11 +0200

    Change default API address to leap.step-lang.dev

--- Leap.Client/Credentials.cs
+++ Leap.Client/Credentials.cs
@@ -2,7 +2,7 @@ namespace Leap.Client;
 
 public record Credentials(string Token, string BaseAddress)
 {
-    public const string DefaultApiBaseAddress = "https://lib.step-lang.dev/api/";
+    public const string DefaultApiBaseAddress = "https://leap.step-lang.dev/api/";
 
     public static Credentials TokenOnly(string token) => new(token, DefaultApiBaseAddress);
 }
