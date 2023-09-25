1385cb99e1ffdd14b9f827be2bdddac94b7d0b87
Ricardo Boss <mail@ricardoboss.de>
2023-09-25 17:30:31 +0200

    Add serilog with two-stage initialization

--- Leap.API/Program.cs
+++ Leap.API/Program.cs
@@ -6,59 +6,87 @@ using Leap.API.Interfaces;
 using Leap.API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddControllers(options => options.Filters.Add<NullResultToNotFoundFilter>());
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<LeapApiDbContext>(options => LeapApiDbContextFactory.ConfigureDbContext(options, builder.Configuration));
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console());
 
-builder.Services.AddSingleton<IPasswordHasher<Author>, PasswordHasher<Author>>();
-builder.Services.AddSingleton<ILibraryStorage, FilesystemLibraryStorage>();
-builder.Services.AddScoped<ITokenGenerator, TokenGenerator>();
+    builder.Services.AddControllers(options => options.Filters.Add<NullResultToNotFoundFilter>());
 
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new()
+    // Add services to the container.
+    builder.Services.AddDbContext<LeapApiDbContext>(options => LeapApiDbContextFactory.ConfigureDbContext(options, builder.Configuration));
+
+    builder.Services.AddSingleton<IPasswordHasher<Author>, PasswordHasher<Author>>();
+    builder.Services.AddSingleton<ILibraryStorage, FilesystemLibraryStorage>();
+    builder.Services.AddScoped<ITokenGenerator, TokenGenerator>();
+
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
         {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration.GetJwtIssuer(),
-            ValidAudience = builder.Configuration.GetJwtAudience(),
-            IssuerSigningKey = builder.Configuration.GetJwtSecretKey(),
-        };
-    });
+            options.TokenValidationParameters = new()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration.GetJwtIssuer(),
+                ValidAudience = builder.Configuration.GetJwtAudience(),
+                IssuerSigningKey = builder.Configuration.GetJwtSecretKey(),
+            };
+        });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+    var app = builder.Build();
 
-await using (var scope = app.Services.CreateAsyncScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<LeapApiDbContext>();
+    app.UseSerilogRequestLogging();
 
-    await context.InitializeAsync();
+    await using (var scope = app.Services.CreateAsyncScope())
+    {
+        var context = scope.ServiceProvider.GetRequiredService<LeapApiDbContext>();
+
+        await context.InitializeAsync();
+    }
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+catch (Exception ex)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Fatal(ex, "Application terminated unexpectedly");
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+finally
+{
+    Log.CloseAndFlush();
+}
