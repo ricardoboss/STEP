f875a7462df86d3e2d07e20f6a1630fe209abfbf
Ricardo Boss <contact@ricardoboss.de>
2023-10-25 01:32:51 +0200

    Refactor unary expressions

--- StepLang/Expressions/UnaryExpression.cs
+++ StepLang/Expressions/UnaryExpression.cs
@@ -3,45 +3,26 @@ using StepLang.Interpreting;
 
 namespace StepLang.Expressions;
 
-public class UnaryExpression : Expression
+public abstract class UnaryExpression : Expression
 {
-    public static Expression Not(Expression expression)
-    {
-        return new UnaryExpression("!", expression, result =>
-        {
-            var value = result.ExpectBool().Value;
-
-            return new BoolResult(!value);
-        });
-    }
-
-    public static Expression Negate(Expression expression)
-    {
-        return new UnaryExpression("-", expression, result =>
-        {
-            var value = result.ExpectNumber().Value;
-
-            return new NumberResult(-value);
-        });
-    }
-
-    private readonly string debugName;
+    private readonly string unaryExpressionSymbol;
     private readonly Expression expression;
-    private readonly Func<ExpressionResult, ExpressionResult> transform;
 
-    public UnaryExpression(string debugName, Expression expression, Func<ExpressionResult, ExpressionResult> transform)
+    protected UnaryExpression(string unaryExpressionSymbol, Expression expression)
     {
-        this.debugName = debugName;
+        this.unaryExpressionSymbol = unaryExpressionSymbol;
         this.expression = expression;
-        this.transform = transform;
     }
 
-    public override async Task<ExpressionResult> EvaluateAsync(Interpreter interpreter, CancellationToken cancellationToken = default)
+    public override async Task<ExpressionResult> EvaluateAsync(Interpreter interpreter,
+        CancellationToken cancellationToken = default)
     {
         var result = await expression.EvaluateAsync(interpreter, cancellationToken);
 
-        return transform.Invoke(result);
+        return Transform(result);
     }
 
-    protected override string DebugDisplay() => $"({debugName} ({expression})";
+    protected abstract ExpressionResult Transform(ExpressionResult result);
+
+    protected override string DebugDisplay() => $"({unaryExpressionSymbol} ({expression})";
 }
