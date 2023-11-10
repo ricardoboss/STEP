ede64afde0a661f0142b9242e63e25509c5d9b07
Ricardo Boss <contact@ricardoboss.de>
2023-11-10 20:45:53 +0100

    Added extension methods for accessing result types of variable declarations

--- StepLang/Parsing/IVariableDeclarationNode.cs
+++ StepLang/Parsing/IVariableDeclarationNode.cs
@@ -8,13 +8,29 @@ public interface IVariableDeclarationNode : IEvaluatableNode<IVariableDeclaratio
 {
     IEnumerable<Token> Types { get; }
 
-    IEnumerable<ResultType> ResultTypes => Types
-        .Where(t => t.Type == TokenType.TypeName)
-        .Select(t => Expressions.Results.ResultTypes.FromTypeName(t.Value));
-
     Token Identifier { get; }
 
     bool HasValue { get; }
+}
+
+public static class VariableDeclarationNodeExtensions
+{
+    public static IEnumerable<ResultType> GetResultTypes(this IVariableDeclarationNode node)
+    {
+        return node.Types
+            .Where(t => t.Type == TokenType.TypeName)
+            .Select(t => ResultTypes.FromTypeName(t.Value));
+    }
+
+    public static bool HasResultType(this IVariableDeclarationNode node, ResultType type)
+    {
+        return node.GetResultTypes().Contains(type);
+    }
+
+    public static string ResultTypesToString(this IVariableDeclarationNode node)
+    {
+        return string.Join("|", node.GetResultTypes().Select(t => t.ToTypeName()));
+    }
 }
 
 public static class VariableDeclarationNodeExtensions
