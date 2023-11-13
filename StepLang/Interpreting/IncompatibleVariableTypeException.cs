10cd5c87d0f996a3d7a23a49086bf0c403d37b9e
Ricardo Boss <contact@ricardoboss.de>
2023-11-13 01:13:27 +0100

    Updated help text

--- StepLang/Interpreting/IncompatibleVariableTypeException.cs
+++ StepLang/Interpreting/IncompatibleVariableTypeException.cs
@@ -5,7 +5,7 @@ namespace StepLang.Interpreting;
 
 public class IncompatibleVariableTypeException : InterpreterException
 {
-    public IncompatibleVariableTypeException(TokenLocation location, Variable variable, ExpressionResult newValue) : base(6, location, $"Cannot assign a value of type {newValue.ResultType.ToTypeName()} to a variable declared as {variable.TypeString}", $"Make sure the value you are trying to assign to {variable.Identifier} is of type {variable.Value.ResultType.ToTypeName()}.")
+    public IncompatibleVariableTypeException(TokenLocation location, Variable variable, ExpressionResult newValue) : base(6, location, $"Cannot assign a value of type {newValue.ResultType.ToTypeName()} to a variable declared as {variable.TypeString}", $"Make sure the value you are trying to assign to {variable.Identifier} is of type {variable.TypeString}.")
     {
     }
 }
