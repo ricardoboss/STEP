32bbdbad270dce28d50ea702596b0119db860169
Ricardo Boss <mail@ricardoboss.de>
2023-11-19 14:08:47 +0100

    Fixed another rebase error

--- StepLang/Parsing/AddExpressionNode.cs
+++ StepLang/Parsing/AddExpressionNode.cs
@@ -5,8 +5,6 @@ namespace StepLang.Parsing;
 
 public record AddExpressionNode(TokenLocation OperatorLocation, ExpressionNode Left, ExpressionNode Right) : BinaryExpressionNode(OperatorLocation, Left, Right, BinaryExpressionOperator.Add)
 {
-    public BinaryExpressionOperator Op => BinaryExpressionOperator.Add;
-
     public override ExpressionResult EvaluateUsing(IExpressionEvaluator evaluator)
     {
         return evaluator.Evaluate(this);
