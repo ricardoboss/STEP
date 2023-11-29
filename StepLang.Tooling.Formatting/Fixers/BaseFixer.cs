e4f351e227fb52ad270c3fb59a94e943d9bfb51d
Ricardo Boss <contact@ricardoboss.de>
2023-11-30 00:43:22 +0100

    Reduce number of state machines by directly returning tasks instead of awaiting them

--- StepLang.Tooling.Formatting/Fixers/BaseFixer.cs
+++ StepLang.Tooling.Formatting/Fixers/BaseFixer.cs
@@ -10,23 +10,23 @@ public abstract class BaseFixer : IFixer
 
     public virtual bool ThrowOnFailure { get; init; } = true;
 
-    public async Task<FixerResult> FixAsync(IAnalyzer analyzer, DirectoryInfo directory,
+    public Task<FixerResult> FixAsync(IAnalyzer analyzer, DirectoryInfo directory,
         CancellationToken cancellationToken = default) =>
-        await FixAsync(new[] { analyzer }, new[] { directory }, cancellationToken);
+        FixAsync(new[] { analyzer }, new[] { directory }, cancellationToken);
 
-    public async Task<FixerResult> FixAsync(IEnumerable<IAnalyzer> analyzers, DirectoryInfo directory,
+    public Task<FixerResult> FixAsync(IEnumerable<IAnalyzer> analyzers, DirectoryInfo directory,
         CancellationToken cancellationToken = default) =>
-        await FixAsync(analyzers, new[] { directory }, cancellationToken);
+        FixAsync(analyzers, new[] { directory }, cancellationToken);
 
-    public async Task<FixerResult> FixAsync(IAnalyzer analyzer, IEnumerable<FileInfo> files,
+    public Task<FixerResult> FixAsync(IAnalyzer analyzer, IEnumerable<FileInfo> files,
         CancellationToken cancellationToken = default) =>
-        await FixAsync(new[] { analyzer }, files, cancellationToken);
+        FixAsync(new[] { analyzer }, files, cancellationToken);
 
-    public async Task<FixerResult> FixAsync(IAnalyzer analyzer, IEnumerable<DirectoryInfo> directories,
+    public Task<FixerResult> FixAsync(IAnalyzer analyzer, IEnumerable<DirectoryInfo> directories,
         CancellationToken cancellationToken = default) =>
-        await FixAsync(new[] { analyzer }, directories, cancellationToken);
+        FixAsync(new[] { analyzer }, directories, cancellationToken);
 
-    public async Task<FixerResult> FixAsync(IEnumerable<IAnalyzer> analyzers,
+    public Task<FixerResult> FixAsync(IEnumerable<IAnalyzer> analyzers,
         IEnumerable<DirectoryInfo> directories, CancellationToken cancellationToken = default)
     {
         var files = directories.SelectMany(d => d.EnumerateFiles(DefaultSearchPattern, new EnumerationOptions
@@ -34,7 +34,7 @@ public abstract class BaseFixer : IFixer
             RecurseSubdirectories = true,
         }));
 
-        return await FixAsync(analyzers, files, cancellationToken);
+        return FixAsync(analyzers, files, cancellationToken);
     }
 
     public async Task<FixerResult> FixAsync(IEnumerable<IAnalyzer> analyzers, IEnumerable<FileInfo> files,
@@ -104,9 +104,9 @@ public abstract class BaseFixer : IFixer
         return fixApplicatorResult with { Elapsed = fixApplicatorResult.Elapsed + runDuration };
     }
 
-    private static async Task<FileAnalysisResult> RunFileAnalyzer(IFileAnalyzer analyzer, FileInfo file, CancellationToken cancellationToken)
+    private static Task<FileAnalysisResult> RunFileAnalyzer(IFileAnalyzer analyzer, FileInfo file, CancellationToken cancellationToken)
     {
-        return await analyzer.AnalyzeAsync(file, cancellationToken);
+        return analyzer.AnalyzeAsync(file, cancellationToken);
     }
 
     private static async Task<StringAnalysisResult> RunStringAnalyzer(IStringAnalyzer analyzer, FileSystemInfo file, CancellationToken cancellationToken)
