c7931e8180b55a87aac798759956c18ff07d5060
Ricardo Boss <mail@ricardoboss.de>
2024-12-10 01:10:18 +0100

    Adjusted expected error message as it now includes the location

--- StepLang.Tests/Interpreting/ScopeTest.cs
+++ StepLang.Tests/Interpreting/ScopeTest.cs
@@ -13,6 +13,6 @@ public class ScopeTest
 		var e = Assert.Throws<UndefinedIdentifierException>(() =>
 			scope.GetVariable(new Token(TokenType.Identifier, "undefined")));
 
-		Assert.Equal("Variable 'undefined' was not declared", e.Message);
+		Assert.Equal("Variable 'undefined' was not declared (used at 1:1)", e.Message);
 	}
 }
