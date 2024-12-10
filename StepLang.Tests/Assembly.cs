949542fb4ae4c2b01da97935cbad00ced1a9d48a
Ricardo Boss <mail@ricardoboss.de>
2024-12-11 00:37:15 +0100

    Suppress CA1515 instead of CA1812 as that is the actual diagnostic changing public to internal

--- StepLang.Tests/Assembly.cs
+++ StepLang.Tests/Assembly.cs
@@ -3,4 +3,4 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: ExcludeFromCodeCoverage]
 [assembly: SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task")]
 [assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods")]
-[assembly: SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Scope = "NamespaceAndDescendants")]
+[assembly: SuppressMessage("Maintainability", "CA1515:Consider making public types internal")]
