2ad41d2400710c53ded7eacb51bd863f8e91372d
Ricardo Boss <mail@ricardoboss.de>
2024-12-11 00:48:22 +0100

    Simplify test project setups

--- StepLang.Tests/Assembly.cs
+++
@@ -1,6 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: ExcludeFromCodeCoverage]
-[assembly: SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task")]
-[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods")]
-[assembly: SuppressMessage("Maintainability", "CA1515:Consider making public types internal")]
--- StepLang.Tooling.Formatting.Tests/Assembly.cs
+++
@@ -1,4 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: ExcludeFromCodeCoverage]
-[assembly: SuppressMessage("Maintainability", "CA1515:Consider making public types internal")]
