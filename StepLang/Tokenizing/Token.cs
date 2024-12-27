6b58e0b6e7629c033df74d27a9bdea1f1785bccd
Ricardo Boss <mail@ricardoboss.de>
2024-12-27 03:15:43 +0100

    Formatting

--- StepLang/Tokenizing/Token.cs
+++ StepLang/Tokenizing/Token.cs
@@ -4,32 +4,32 @@ namespace StepLang.Tokenizing;
 
 public class Token
 {
-    public TokenType Type { get; }
+	public TokenType Type { get; }
 
-    public string Value { get; }
+	public string Value { get; }
 
-    public TokenLocation Location { get; }
+	public TokenLocation Location { get; }
 
-    public Token(TokenType type, string value, TokenLocation? location = null)
-    {
-        Type = type;
-        Value = value;
-        Location = location ?? new TokenLocation();
-    }
+	public Token(TokenType type, string value, TokenLocation? location = null)
+	{
+		Type = type;
+		Value = value;
+		Location = location ?? new TokenLocation();
+	}
 
-    public string StringValue => Type switch
-    {
-        TokenType.LiteralString => Value[1..^1], // cut off the quotes
-        _ => Value,
-    };
+	public string StringValue => Type switch
+	{
+		TokenType.LiteralString => Value[1..^1], // cut off the quotes
+		_ => Value,
+	};
 
-    [ExcludeFromCodeCoverage]
-    public override string ToString()
-    {
-        var printableValue = Value
-            .Replace("\n", "\\n", StringComparison.InvariantCulture)
-            .Replace("\r", "\\r", StringComparison.InvariantCulture);
+	[ExcludeFromCodeCoverage]
+	public override string ToString()
+	{
+		var printableValue = Value
+			.Replace("\n", "\\n", StringComparison.InvariantCulture)
+			.Replace("\r", "\\r", StringComparison.InvariantCulture);
 
-        return Value.Length > 0 ? $"<{Type}: '{printableValue}'>" : $"<{Type}>";
-    }
+		return Value.Length > 0 ? $"<{Type}: '{printableValue}'>" : $"<{Type}>";
+	}
 }
--- StepLang/Tokenizing/TokenLocation.cs
+++ StepLang/Tokenizing/TokenLocation.cs
@@ -2,26 +2,26 @@ namespace StepLang.Tokenizing;
 
 public record TokenLocation(Uri? DocumentUri = null, int Line = 1, int Column = 1, int Length = 0)
 {
-    /// <inheritdoc />
-    public override string ToString()
-    {
-        if (File is {} file)
-            return $"{file.Name}:{Line}:{Column}";
+	/// <inheritdoc />
+	public override string ToString()
+	{
+		if (File is { } file)
+			return $"{file.Name}:{Line}:{Column}";
 
-        if (DocumentUri != null)
-            return $"{DocumentUri.ToString()}:{Line}:{Column}";
+		if (DocumentUri != null)
+			return $"{DocumentUri.ToString()}:{Line}:{Column}";
 
-        return $"{Line}:{Column}";
-    }
+		return $"{Line}:{Column}";
+	}
 
-    public FileSystemInfo? File
-    {
-        get
-        {
-            if (DocumentUri is null)
-                return null;
+	public FileSystemInfo? File
+	{
+		get
+		{
+			if (DocumentUri is null)
+				return null;
 
-            return DocumentUri.IsFile ? new FileInfo(DocumentUri.AbsolutePath) : null;
-        }
-    }
+			return DocumentUri.IsFile ? new FileInfo(DocumentUri.AbsolutePath) : null;
+		}
+	}
 }
