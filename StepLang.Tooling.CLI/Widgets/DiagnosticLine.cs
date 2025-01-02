e55f04ef8400dd9975f0ffaab6af9ba492203426
Ricardo Boss <mail@ricardoboss.de>
2025-01-02 13:32:46 +0100

    Put all text into a single paragraph

--- StepLang.Tooling.CLI/Widgets/DiagnosticLine.cs
+++ StepLang.Tooling.CLI/Widgets/DiagnosticLine.cs
@@ -1,60 +1,19 @@
 using Spectre.Console;
 using Spectre.Console.Rendering;
 using StepLang.Diagnostics;
-using System.Globalization;
 using System.Text;
 
 namespace StepLang.Tooling.CLI.Widgets;
 
-public class DiagnosticLine(Diagnostic diagnostic) : IRenderable
+public class DiagnosticLine : Renderable
 {
-	private readonly TextPath path = new(diagnostic.File?.FullName ?? "unknown") { LeafStyle = "bold" };
-	private readonly Style severityStyle = diagnostic.Severity switch
+	private readonly TextPath path;
+	private readonly Paragraph text;
+
+	public DiagnosticLine(Diagnostic diagnostic)
 	{
-		Severity.Error => "bold red",
-		Severity.Warning => "bold yellow",
-		Severity.Suggestion => "aqua",
-		Severity.Hint => "blue",
-		_ => "default",
-	};
-	private readonly string severityLabel = diagnostic.Severity switch
-	{
-		Severity.Error => "error",
-		Severity.Warning => "warning",
-		Severity.Suggestion => "suggestion",
-		Severity.Hint => "hint",
-		_ => "",
-	};
-
-	public Measurement Measure(RenderOptions options, int maxWidth)
-	{
-		var pathMeasurement = path.Measure(options, maxWidth);
-
-		var locationWidth = 1; // (
-		locationWidth += diagnostic.Line.ToString(CultureInfo.InvariantCulture).Length;
-		locationWidth++; // ,
-		locationWidth += diagnostic.Column.ToString(CultureInfo.InvariantCulture).Length;
-		locationWidth++; // )
-		locationWidth++; // :
-		locationWidth++; // space
-
-		var severityWidth = severityLabel.Length;
-		severityWidth++; // space
-		severityWidth += diagnostic.Code.Length;
-		severityWidth++; // :
-		severityWidth++; // space
-
-		var messageWidth = diagnostic.Message.Length;
-
-		var totalWidth = pathMeasurement.Max + locationWidth + severityWidth + messageWidth;
-
-		return new Measurement(totalWidth, totalWidth);
-	}
-
-	public IEnumerable<Segment> Render(RenderOptions options, int maxWidth)
-	{
-		foreach (var segment in path.Render(options, maxWidth))
-			yield return segment;
+		path = new TextPath(diagnostic.File?.FullName ?? "unknown") { LeafStyle = "bold" };
+		text = new Paragraph();
 
 		var sb = new StringBuilder();
 		sb.Append('(');
@@ -62,18 +21,55 @@ public class DiagnosticLine(Diagnostic diagnostic) : IRenderable
 		sb.Append(',');
 		sb.Append(diagnostic.Column);
 		sb.Append("): ");
-		yield return new Segment(sb.ToString());
+		_ = text.Append(sb.ToString());
+
+		var severityStyle = diagnostic.Severity switch
+		{
+			Severity.Error => "bold red",
+			Severity.Warning => "bold yellow",
+			Severity.Suggestion => "aqua",
+			Severity.Hint => "blue",
+			_ => "default",
+		};
+
+		var severityLabel = diagnostic.Severity switch
+		{
+			Severity.Error => "error",
+			Severity.Warning => "warning",
+			Severity.Suggestion => "suggestion",
+			Severity.Hint => "hint",
+			_ => "",
+		};
 
 		sb.Clear();
 		sb.Append(severityLabel);
 		sb.Append(' ');
 		sb.Append(diagnostic.Code);
-		yield return new Segment(sb.ToString(), severityStyle);
+		_ = text.Append(sb.ToString(), severityStyle);
 
 		sb.Clear();
 		sb.Append(": ");
 		sb.Append(diagnostic.Message);
-		yield return new Segment(sb.ToString());
+		_ = text.Append(sb.ToString());
+	}
+
+	protected override Measurement Measure(RenderOptions options, int maxWidth)
+	{
+		var pathMeasurement = path.Measure(options, maxWidth);
+		var textMeasurement = ((IRenderable)text).Measure(options, maxWidth - pathMeasurement.Max);
+
+		var totalWidth = pathMeasurement.Max + textMeasurement.Max;
+
+		return new Measurement(totalWidth, totalWidth);
+	}
+
+	protected override IEnumerable<Segment> Render(RenderOptions options, int maxWidth)
+	{
+		foreach (var segment in path.Render(options, maxWidth))
+			yield return segment;
+
+		foreach (var segment in ((IRenderable)text).Render(options, maxWidth))
+			yield return segment;
 
 		yield return Segment.LineBreak;
 	}
