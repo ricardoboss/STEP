30a25566bfded6f8f93567e8e77592917695d1ee
Ricardo Boss <mail@ricardoboss.de>
2025-01-08 02:50:18 +0100

    Disable warning because I like this API

--- StepLang/Utils/Result.cs
+++ StepLang/Utils/Result.cs
@@ -2,9 +2,11 @@ namespace StepLang.Utils;
 
 public abstract class Result<T>
 {
-	public static Ok<T> Ok(T value) => new(value);
+#pragma warning disable CA1000 # Do not declare static members on generic types
+	public static Result<T> Ok(T value) => new Ok<T>(value);
 
-	public static Err<T> Err(Exception e) => new(e);
+	public static Result<T> Err(Exception e) => new Err<T>(e);
+#pragma warning restore CA1000 # Do not declare static members on generic types
 
 	public abstract bool IsOk { get; }
 
