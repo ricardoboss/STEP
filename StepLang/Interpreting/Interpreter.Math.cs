be43dc5d62c2d8cc750a921748b06ba5ac966b37
Ricardo Boss <mail@ricardoboss.de>
2025-04-13 16:01:03 +0200

    Rename property so it doesn't conflict with language keyword

--- StepLang/Interpreting/Interpreter.Math.cs
+++ StepLang/Interpreting/Interpreter.Math.cs
@@ -12,7 +12,7 @@ public partial class Interpreter
 	{
 		var leftResult = expressionNode.Left.EvaluateUsing(this);
 		var rightResult = expressionNode.Right.EvaluateUsing(this);
-		var location = expressionNode.Operator.Location;
+		var location = expressionNode.OperatorToken.Location;
 
 		return (leftResult, rightResult, location);
 	}
