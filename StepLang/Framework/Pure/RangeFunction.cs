7455569019123c7cec285e2be16b35da565436b6
Ricardo Boss <mail@ricardoboss.de>
2025-10-03 21:10:38 +0200

    Formatting and simpler loop to generate values

--- StepLang/Framework/Pure/RangeFunction.cs
+++ StepLang/Framework/Pure/RangeFunction.cs
@@ -12,15 +12,15 @@ public class RangeFunction : GenericFunction<NumberResult, NumberResult, NumberR
 
 	protected override IEnumerable<NativeParameter> NativeParameters { get; } =
 	[
-			new(OnlyNumber, "start"),
-				new(OnlyNumber, "end"),
-				new(OnlyNumber, "step", LiteralExpressionNode.FromInt32(1)),
-		];
+		new(OnlyNumber, "start"),
+		new(OnlyNumber, "end"),
+		new(OnlyNumber, "step", LiteralExpressionNode.FromInt32(1)),
+	];
 
 	protected override IEnumerable<ResultType> ReturnTypes { get; } = OnlyList;
 
-	protected override ExpressionResult Invoke(TokenLocation callLocation, Interpreter interpreter, NumberResult argument1,
-			NumberResult argument2, NumberResult argument3)
+	protected override ExpressionResult Invoke(TokenLocation callLocation, Interpreter interpreter,
+		NumberResult argument1, NumberResult argument2, NumberResult argument3)
 	{
 		var startValue = Convert.ToDecimal(argument1.Value);
 		var endValue = Convert.ToDecimal(argument2.Value);
@@ -34,46 +34,25 @@ public class RangeFunction : GenericFunction<NumberResult, NumberResult, NumberR
 		if (stepValue > 0 && startValue > endValue)
 		{
 			throw new InvalidArgumentValueException(callLocation,
-					"Positive step requires start to be less than or equal to end");
+				"Positive step requires start to be less than or equal to end");
 		}
 
 		if (stepValue < 0 && startValue < endValue)
 		{
 			throw new InvalidArgumentValueException(callLocation,
-					"Negative step requires start to be greater than or equal to end");
+				"Negative step requires start to be greater than or equal to end");
 		}
 
 		var values = new List<ExpressionResult>();
 
-		var epsilon = Math.Abs(stepValue) / 1_000_000_000m;
+		var maxSteps = (int)Math.Truncate(Math.Abs(endValue - startValue) / Math.Abs(stepValue));
+		for (var currentStep = 0; currentStep <= maxSteps; currentStep++)
+		{
+			var currentValue = startValue + stepValue * currentStep;
 
-		if (stepValue > 0)
-		{
-			for (var current = startValue; current <= endValue + epsilon; current += stepValue)
-			{
-				var value = AdjustForEndpoint(current, endValue, epsilon);
-				values.Add(NumberResult.FromDouble(decimal.ToDouble(value)));
-			}
-		}
-		else
-		{
-			for (var current = startValue; current >= endValue - epsilon; current += stepValue)
-			{
-				var value = AdjustForEndpoint(current, endValue, epsilon);
-				values.Add(NumberResult.FromDouble(decimal.ToDouble(value)));
-			}
+			values.Add(Convert.ToDouble(currentValue).ToNumberResult());
 		}
 
 		return new ListResult(values);
-	}
-
-	private static decimal AdjustForEndpoint(decimal current, decimal endValue, decimal epsilon)
-	{
-		if (Math.Abs(current - endValue) <= epsilon)
-		{
-			return endValue;
-		}
-
-		return current;
 	}
 }
