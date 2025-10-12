f333ebff58b56ba58a190f0d3a8f65d0f75bf76f
Ricardo Boss <mail@ricardoboss.de>
2025-10-12 02:03:32 +0200

    New VS Code extension

---
+++ extension/src/extension.ts
@@ -0,0 +1,28 @@
+import * as vscode from "vscode";
+import { LanguageClient, LanguageClientOptions, StreamInfo, Trace } from "vscode-languageclient/node";
+import * as net from "node:net";
+
+let client: LanguageClient | undefined;
+
+export async function activate(context: vscode.ExtensionContext) {
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "STEP", pattern: "*.step" }],
+  };
+
+  const serverOptions = () =>
+    new Promise<StreamInfo>((resolve, reject) => {
+      const socket = net.connect(14246, "127.0.0.1", () => {
+        resolve({ writer: socket, reader: socket });
+      });
+      socket.on("error", reject);
+    });
+
+  const client = new LanguageClient("step", "STEP LSP Client", serverOptions, clientOptions);
+  await client.start();
+
+  context.subscriptions.push({ dispose: () => client.stop() });
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	return client?.stop();
+}
