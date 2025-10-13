8d2cf5c18b2a5f2a5408d034d85472f6d37e3d8b
Ricardo Boss <mail@ricardoboss.de>
2025-10-13 17:16:04 +0200

    Added another test for usages collection

--- StepLang.Tests/Diagnostics/VariableDeclarationCollectorTest.cs
+++ StepLang.Tests/Diagnostics/VariableDeclarationCollectorTest.cs
@@ -1,4 +1,5 @@
 using StepLang.Diagnostics;
+using StepLang.Tokenizing;
 
 namespace StepLang.Tests.Diagnostics;
 
@@ -17,5 +18,32 @@ public static class VariableDeclarationCollectorTest
 
 		Assert.That(collector.Declarations, Has.Count.EqualTo(1));
 		Assert.That(collector.Declarations[0].Name, Is.EqualTo("iamunused"));
+		Assert.That(collector.Scopes, Has.Count.EqualTo(1));
+		Assert.That(collector.Scopes[0].Usages, Is.Empty);
+	}
+
+	[Test]
+	public static void TestFindsSimpleUsageAsParam()
+	{
+		const string sourceCode = """
+		                          string foo = "hello"
+		                          println(foo)
+		                          """;
+
+		var documentUri = new Uri("file:///path/to/file.step");
+		var root = sourceCode.AsParsed();
+
+		var collector = new VariableDeclarationCollector(documentUri);
+		collector.Visit(root);
+
+		Assert.That(collector.Declarations, Has.Count.EqualTo(1));
+		Assert.That(collector.Declarations[0].Name, Is.EqualTo("foo"));
+		Assert.That(collector.Scopes, Has.Count.EqualTo(1));
+		Assert.That(collector.Scopes[0].Usages, Contains.Key("foo"));
+
+		var usage = collector.Scopes[0].Usages["foo"];
+		Assert.That(usage, Has.Count.EqualTo(1));
+		Assert.That(usage[0].Type, Is.EqualTo(TokenType.Identifier));
+		Assert.That(usage[0].Value, Is.EqualTo("foo"));
 	}
 }
