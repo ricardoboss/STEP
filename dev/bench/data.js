66415a79da38a9dd7cc106f4ce0aaf9e8a43fe2e
github-action-benchmark <github@users.noreply.github.com>
2025-12-01 20:20:56 +0000

    add Benchmark (benchmarkdotnet) benchmark result for be23ecba0c59c40568c424a2bec1be9056b96ffc

---
+++ dev/bench/data.js
@@ -0,0 +1,64 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1764620456447,
+  "repoUrl": "https://github.com/ricardoboss/STEP",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "ricardoboss",
+            "username": "ricardoboss"
+          },
+          "committer": {
+            "name": "ricardoboss",
+            "username": "ricardoboss"
+          },
+          "id": "be23ecba0c59c40568c424a2bec1be9056b96ffc",
+          "message": "Add benchmarks",
+          "timestamp": "2025-12-01T18:57:47Z",
+          "url": "https://github.com/ricardoboss/STEP/pull/195/commits/be23ecba0c59c40568c424a2bec1be9056b96ffc"
+        },
+        "date": 1764620455943,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "StepLang.Benchmarks.ExampleFileBenchmark.Tokenize(ExampleFileName: \"expressions.step\")",
+            "value": 196.14928800719125,
+            "unit": "ns",
+            "range": "± 0.8103127091186126"
+          },
+          {
+            "name": "StepLang.Benchmarks.ExampleFileBenchmark.Parse(ExampleFileName: \"expressions.step\")",
+            "value": 72275.47635091146,
+            "unit": "ns",
+            "range": "± 1014.6046435898655"
+          },
+          {
+            "name": "StepLang.Benchmarks.ExampleFileBenchmark.Interpret(ExampleFileName: \"expressions.step\")",
+            "value": 1825.0054128011068,
+            "unit": "ns",
+            "range": "± 12.423527916835141"
+          },
+          {
+            "name": "StepLang.Benchmarks.ExampleFileBenchmark.Tokenize(ExampleFileName: \"math.step\")",
+            "value": 192.68175571759542,
+            "unit": "ns",
+            "range": "± 2.1984427960565998"
+          },
+          {
+            "name": "StepLang.Benchmarks.ExampleFileBenchmark.Parse(ExampleFileName: \"math.step\")",
+            "value": 95050.81279296875,
+            "unit": "ns",
+            "range": "± 1222.2513874635197"
+          },
+          {
+            "name": "StepLang.Benchmarks.ExampleFileBenchmark.Interpret(ExampleFileName: \"math.step\")",
+            "value": 3001.661644236247,
+            "unit": "ns",
+            "range": "± 15.41632552442601"
+          }
+        ]
+      }
+    ]
+  }
+}
